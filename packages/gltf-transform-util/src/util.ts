69fbfddece0e8e09df94a2709cf389330807774a
Don McCurdy <don.r.mccurdy@gmail.com>
2018-10-22 22:03:13 -0700

    Basic GLB parsing.

--- packages/gltf-transform-util/src/util.ts
+++ packages/gltf-transform-util/src/util.ts
@@ -19,7 +19,35 @@ class GLTFUtil {
    * @param glb
    */
   static wrapGLB(glb: ArrayBuffer): GLTFContainer {
-    throw new Error('Not implemented.');
+    // Decode and verify GLB header.
+    const header = new Uint32Array(glb, 0, 3);
+    if (header[0] !== 0x46546C67) {
+      throw new Error('Invalid glTF asset.');
+    } else if (header[1] !== 2) {
+      throw new Error(`Unsupported glTF binary version, "${header[1]}".`);
+    }
+
+    // Decode and verify chunk headers.
+    const jsonChunkHeader = new Uint32Array(glb, 12, 2);
+    const binaryChunkHeader = new Uint32Array(glb, 12 + 8 + jsonChunkHeader[0], 2);
+    if (jsonChunkHeader[1] !== 0x4E4F534A || binaryChunkHeader[1] !== 0x004E4942) {
+      throw new Error('Unexpected GLB layout.');
+    }
+
+    // Decode content.
+    const jsonByteOffset = 20;
+    const jsonByteLength = jsonChunkHeader[0];
+    const json = JSON.parse(this.decodeText(glb.slice(jsonByteOffset, jsonByteOffset + jsonByteLength)));
+    const binaryByteOffset = 12 + 8 + jsonByteLength;
+    const binaryByteLength = binaryChunkHeader[0];
+    const binary = glb.slice(binaryByteOffset, binaryByteOffset + binaryByteLength);
+    const buffer = json.buffers.find((b) => !b.uri);
+    buffer.uri = 'resources.bin';
+
+    // TODO(donmccurdy): Unpack embedded textures.
+    // TODO(donmccurdy): Decode Draco content.
+
+    return new GLTFContainer(json, {'resources.bin': binary});
   }
 
   /**
@@ -73,6 +101,21 @@ class GLTFUtil {
 
   static createLogger(name: string, verbosity: LoggerVerbosity): Logger {
     return new Logger(name, verbosity);
+  }
+
+  static encodeText(text: string): ArrayBuffer {
+    if (typeof TextEncoder !== 'undefined') {
+      return new TextEncoder().encode(text);
+    }
+    return Buffer.from(text).buffer;
+  }
+
+  static decodeText(buffer: ArrayBuffer): string {
+    if (typeof TextDecoder !== 'undefined') {
+      return new TextDecoder().decode(buffer);
+    }
+    const a = Buffer.from(buffer) as any; 
+    return a.toString('utf8');
   }
 }
 
--- packages/gltf-transform-util/test/test.js
+++ packages/gltf-transform-util/test/test.js
@@ -1,19 +1,25 @@
+const fs = require('fs');
 const test = require('tape');
 const glob = require('glob');
 const path = require('path');
-const load = require('load-json-file');
-const write = require('write-json-file');
+// const load = require('load-json-file');
+// const write = require('write-json-file');
 const { GLTFUtil, GLTFContainer } = require('../');
 test('gltf-transform-util', t => {
-  glob.sync(path.join(__dirname, 'test', 'in', '*.json')).forEach(filepath => {
-    // Define params
+  glob.sync(path.join(__dirname, 'in', '*.glb')).forEach(filepath => {
+    
     const {name} = path.parse(filepath);
-    const gltf = load.sync(filepath);
+    const glbBuffer = fs.readFileSync(filepath);
+    // Byte offset may be non-zero.
+    const {byteOffset, byteLength} = glbBuffer;
+    const glb = glbBuffer.buffer.slice(byteOffset, byteOffset + byteLength);
+    const container = GLTFUtil.wrapGLB(glb);
+    console.log(container.json);
     // etc.
-    const results = [];
-    const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
-    if (process.env.REGEN) write.sync(out, results);
-    t.deepEqual(results, load.sync(out), name);
+    // const results = [];
+    // const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
+    // if (process.env.REGEN) write.sync(out, results);
+    // t.deepEqual(results, load.sync(out), name);
   });
   t.end();
 });
