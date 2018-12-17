d09261db2fb2795066c81a09427f84c2761e6e5e
Don McCurdy <don.r.mccurdy@gmail.com>
2018-12-16 18:01:50 -0800

    Add occlusion-vertex tests.

--- packages/gltf-transform-atlas/test/test.js
+++ packages/gltf-transform-atlas/test/test.js
@@ -1,24 +1,13 @@
 require('source-map-support').install();
 
-const test = require('tape');
-const glob = require('glob');
+const fs = require('fs');
 const path = require('path');
-const load = require('load-json-file');
-const write = require('write-json-file');
+const test = require('tape');
 
-const { GLTFUtil } = require('gltf-transform-util');
+const { NodeIO } = require('gltf-transform-util');
 const { atlas } = require('../');
 
 test('gltf-transform-atlas', t => {
-  glob.sync(path.join(__dirname, 'test', 'in', '*.json')).forEach(filepath => {
-    // Define params
-    const {name} = path.parse(filepath);
-    const gltf = load.sync(filepath);
-    // etc.
-    const results = [];
-    const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
-    if (process.env.REGEN) write.sync(out, results);
-    t.deepEqual(results, load.sync(out), name);
-  });
+  // ...
   t.end();
 });
--- packages/gltf-transform-occlusion-vertex/test/test.js
+++ packages/gltf-transform-occlusion-vertex/test/test.js
@@ -1,24 +1,29 @@
 require('source-map-support').install();
 
-const test = require('tape');
-const glob = require('glob');
+const fs = require('fs');
 const path = require('path');
-const load = require('load-json-file');
-const write = require('write-json-file');
+const test = require('tape');
+const gl = require('gl');
 
-const { GLTFUtil, GLTFContainer } = require('gltf-transform-util');
+const { NodeIO } = require('gltf-transform-util');
 const { occlusionVertex } = require('../');
 
+
 test('gltf-transform-occlusion-vertex', t => {
-  glob.sync(path.join(__dirname, 'in', '*.json')).forEach(filepath => {
-    // Define params
-    const {name} = path.parse(filepath);
-    const gltf = load.sync(filepath);
-    // etc.
-    const results = [];
-    const out = filepath.replace(path.join('test', 'in'), path.join('test', 'out'));
-    if (process.env.REGEN) write.sync(out, results);
-    t.deepEqual(results, load.sync(out), name);
-  });
+  const io = new NodeIO(fs, path);
+  const container = io.read(path.join(__dirname, 'in/chr_knight.glb'));
+  const material = container.json.materials[0];
+  const mesh = container.json.meshes[0];
+
+  t.notOk(material.occlusionTexture, 'begins without occlusionTexture');
+  t.notOk(mesh.primitives[0].attributes.TEXCOORD_1, 'begins without TEXCOORD_1');
+  t.equals(container.json.textures.length, 1, 'begins with one texture')
+
+  occlusionVertex(container, {gl});
+
+  t.ok(material.occlusionTexture, 'adds occlusionTexture');
+  t.ok(mesh.primitives[0].attributes.TEXCOORD_1, 'adds TEXCOORD_1');
+  t.equals(container.json.textures.length, 2, 'adds texture')
+
   t.end();
 });
