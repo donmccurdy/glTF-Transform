6646c991c41091dcb4bc4ed68a62e4534861092b
Don McCurdy <don.r.mccurdy@gmail.com>
2018-12-18 23:15:49 -0800

    Add validation script.

---
+++ scripts/validate.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const glob = require('glob');
+const validator = require('gltf-validator');
+
+const messages = {
+    numAssets: 0,
+    numFailed: 0,
+    numErrors: 0,
+    numWarnings: 0,
+    numInfos: 0,
+    numHints: 0,
+    failed: [],
+    reports: []
+};
+
+const pending = [];
+
+const validateURI = (uri) => {
+    const asset = fs.readFileSync(uri);
+    const dir = path.dirname(uri);
+    const promise = validator.validateBytes(new Uint8Array(asset), {
+        externalResourceFunction: (uri) =>
+        new Promise((resolve, reject) => {
+            uri = path.resolve(dir, decodeURIComponent(uri));
+            resolve(fs.readFileSync(uri));
+        })
+    })
+        .then((report) => {
+            messages.numAssets++;
+            messages.numErrors += report.issues.numErrors;
+            messages.numWarnings += report.issues.numWarnings;
+            messages.numInfos += report.issues.numInfos;
+            messages.numHints += report.issues.numHints;
+            messages.reports.push(report);
+            if (report.issues.numErrors || report.issues.numWarnings) {
+                messages.failed.push(uri);
+            }
+        })
+        .catch((error) => {
+            messages.numFailed++;
+            failed.push([uri, error]);
+        });
+    pending.push(promise);
+};
+
+glob.sync(path.join(__dirname, '../packages/*/test/out/**/*.glb')).forEach(validateURI);
+glob.sync(path.join(__dirname, '../packages/*/test/out/**/*.gltf')).forEach(validateURI);
+
+Promise.all(pending)
+    .catch(() => true)
+    .then(() => {
+        console.log(JSON.stringify(messages, null, 2));
+        if (messages.numFailed || messages.numErrors || messages.numWarnings) {
+            process.exit(2);
+        }
+    });
+
+
