24148bf37c4be4eded0f7f076d760ca90bd986ef
Don McCurdy <don.r.mccurdy@gmail.com>
2018-12-21 09:23:31 -0800

    [atlas] Partial implementation.

---
+++ packages/atlas/src/image-util.ts
@@ -0,0 +1,74 @@
+interface ISize {
+    width: number,
+    height: number,
+}
+
+////////////////////////////////////////////////////////
+
+function extractSize (buffer: Buffer, i: number) {
+    return {
+        'height' : buffer.readUInt16BE(i),
+        'width' : buffer.readUInt16BE(i + 2)
+    };
+}
+
+function validateBuffer (buffer: Buffer, i: number) {
+    // index should be within buffer limits
+    if (i > buffer.length) {
+        throw new TypeError('Corrupt JPG, exceeded buffer limits');
+    }
+    // Every JPEG block must begin with a 0xFF
+    if (buffer[i] !== 0xFF) {
+        throw new TypeError('Invalid JPG, marker table corrupted');
+    }
+}
+
+function getSizeJPEG (buffer: Buffer): ISize {
+    // Skip 4 chars, they are for signature
+    buffer = buffer.slice(4);
+
+    var i, next;
+    while (buffer.length) {
+        // read length of the next block
+        i = buffer.readUInt16BE(0);
+
+        // ensure correct format
+        validateBuffer(buffer, i);
+
+        // 0xFFC0 is baseline standard(SOF)
+        // 0xFFC1 is baseline optimized(SOF)
+        // 0xFFC2 is progressive(SOF2)
+        next = buffer[i + 1];
+        if (next === 0xC0 || next === 0xC1 || next === 0xC2) {
+            return extractSize(buffer, i + 5);
+        }
+
+        // move to the next block
+        buffer = buffer.slice(i + 2);
+    }
+
+    throw new TypeError('Invalid JPG, no size found');
+}
+
+////////////////////////////////////////////////////////
+
+var pngSignature = 'PNG\r\n\x1a\n';
+var pngImageHeaderChunkName = 'IHDR';
+
+// Used to detect "fried" png's: http://www.jongware.com/pngdefry.html
+var pngFriedChunkName = 'CgBI';
+
+function getSizePNG (buffer: Buffer): ISize {
+    if (buffer.toString('ascii', 12, 16) === pngFriedChunkName) {
+        return {
+            width: buffer.readUInt32BE(32),
+            height: buffer.readUInt32BE(36)
+        };
+    }
+    return {
+        width: buffer.readUInt32BE(16),
+        height: buffer.readUInt32BE(20)
+    };
+}
+
+export { getSizeJPEG, getSizePNG };
--- packages/atlas/src/index.ts
+++ packages/atlas/src/index.ts
@@ -1,14 +1,68 @@
-import * as atlaspack from 'atlaspack';
+import ShelfPack from '@mapbox/shelf-pack';
 import { GLTFContainer } from '@gltf-transform/core';
+import { getSizePNG, getSizeJPEG } from './image-util';
 
-interface AtlasOptions {};
-
-const DEFAULTS = {
-  maxSize: 2048
+interface IAtlasOptions {
+  size,
+  tilepad: boolean,
+  createCanvas: CanvasFactory,
 };
 
-function atlas(container: GLTFContainer, options: AtlasOptions): GLTFContainer {
-  throw new Error('Not implemented');
+interface CanvasFactory {
+  (w: number, h: number): HTMLCanvasElement;
+}
+
+const DEFAULT_OPTIONS = {
+  size: 512,
+  tilepad: false,
+  bakeUVs: false,
+};
+
+function atlas(container: GLTFContainer, options: IAtlasOptions): GLTFContainer {
+  options = {...DEFAULT_OPTIONS, ...options};
+
+  // Gather base color textures.
+  let baseColorTextures = [];
+  container.json.materials.forEach((material) => {
+    if (!material.pbrMetallicRoughness) return;
+    if (!material.pbrMetallicRoughness.baseColorTexture) return;
+    const baseColorTexture = material.pbrMetallicRoughness.baseColorTexture;
+    baseColorTextures.push(baseColorTexture.index);
+  });
+  baseColorTextures = Array.from(new Set(baseColorTextures));
+
+  // Gather texture resources.
+  const textures = baseColorTextures.map((index) => {
+    const texture = container.json.textures[index];
+    const image = container.json.images[texture.source];
+    const arrayBuffer = container.resolveURI(image.uri);
+    // TODO: Dimensions returned are incorrect.
+    const {width, height} = image.mimeType === 'image/jpeg'
+      ? getSizeJPEG(Buffer.from(arrayBuffer))
+      : getSizePNG(Buffer.from(arrayBuffer));
+    return {index, width, height, arrayBuffer};
+  });
+
+  // Pack textures.
+  const atlas = new ShelfPack(options.size, options.size);
+  textures.sort((a, b) => a.width * a.height > b.width * b.height ? 1 : -1);
+  atlas.pack(textures, {inPlace: true});
+
+  console.log(textures);
+
+  // Write pixels.
+  // let canvas;
+  // if (options.createCanvas) {
+  //   canvas = options.createCanvas(options.size, options.size);
+  // } else {
+  //   canvas = document.createElement('canvas');
+  //   canvas.height = options.size;
+  //   canvas.width = options.size;
+  // }
+
+  // Update texture references.
+
+  return container;
 }
 
 export { atlas };
