05731cdc405f49d9f5445942a1859cbed24554ef
Don McCurdy <don.r.mccurdy@gmail.com>
2019-03-07 22:49:11 -0800

    [colorspace] Test should not require comparing buffers.

--- packages/colorspace/test/test.js
+++ packages/colorspace/test/test.js
@@ -9,10 +9,23 @@ const { colorspace } = require('../');
 
 
 test('@gltf-transform/colorspace', t => {
+  const input = [ 0.25882352941176473, 0.5215686274509804, 0.9568627450980393 ]; // sRGB
+  const expected = [ 0.054480276435339814, 0.23455058215026167, 0.9046611743890203 ]; // linear
+
   const io = new NodeIO(fs, path);
-  const input = io.read(path.join(__dirname, 'in/Parrot.glb'));
-  const expected = io.read(path.join(__dirname, 'out/Parrot.glb'));
-  colorspace(input, {inputEncoding: 'sRGB'});
-  t.ok(input.equals(expected), 'converts vertex colors from sRGB to linear');
+  const container = io.read(path.join(__dirname, 'in/Parrot.glb'));
+
+  const colorAccessorIndex = container.json.meshes[0].primitives[0].attributes['COLOR_0'];
+  console.log('accessor --> ' + colorAccessorIndex);
+  const colorArray = container.getAccessorArray(colorAccessorIndex);
+  colorArray.set(input);
+
+  colorspace(container, {inputEncoding: 'sRGB'});
+
+  const actual = colorArray.slice(0, 4);
+
+  t.equals(actual[0].toFixed(3), expected[0].toFixed(3), 'color.r');
+  t.equals(actual[1].toFixed(3), expected[1].toFixed(3), 'color.g');
+  t.equals(actual[2].toFixed(3), expected[2].toFixed(3), 'color.b');
   t.end();
 });
