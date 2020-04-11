1515ccf04979dc4d4a98d8cca058bd69fa823d7e
Don McCurdy <donmccurdy@google.com>
2020-04-10 18:15:57 -0700

    Revert "v2 Disable most packages."
    This reverts commit ef64dc3cc39c72a02b4c0afa1de475c46c91057d.

---
+++ packages/prune/test/test.js
@@ -0,0 +1,25 @@
+require('source-map-support').install();
+
+const fs = require('fs');
+const path = require('path');
+const test = require('tape');
+
+const { NodeIO } = require ('@gltf-transform/core');
+const { prune } = require('../');
+
+test('@gltf-transform/split', t => {
+  const io = new NodeIO(fs, path);
+  const container = io.read(path.join(__dirname, 'in/many-cubes.gltf'));
+  t.equal(container.json.accessors.length, 1503, 'begins with duplicate accessors');
+  prune(container);
+  t.equal(container.json.accessors.length, 4, 'prunes duplicate accessors');
+
+  const outputURI = path.join(__dirname, 'out/many-cubes.gltf');
+  if (process.env.REGEN) {
+    io.writeGLTF(outputURI, container);
+  } else {
+    const expectedContainer = io.readGLTF(outputURI);
+    t.ok(container.equals(expectedContainer), 'Container matches expected.');
+  }
+  t.end();
+});
