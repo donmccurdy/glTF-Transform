9735379bc4e79b30b5f831d360b17481ae0b2178
Don McCurdy <donmccurdy@google.com>
2020-04-12 19:45:02 -0700

    Test cleanup.

---
+++ packages/core/test/graph.test.js
@@ -0,0 +1,38 @@
+require('source-map-support').install();
+
+const test = require('tape');
+const { Root, Graph, Node } = require('../');
+
+test('@gltf-transform/core::graph | link management', t => {
+	const graph = new Graph();
+	const root = new Root(graph);
+	const a = new Node(graph);
+	const b = new Node(graph);
+
+	root.addNode(a).addNode(b);
+	t.deepEqual(root.listNodes(), [a, b], 'Added two nodes.');
+
+	root.removeNode(a);
+	t.deepEqual(root.listNodes(), [b], 'Removed a node.');
+
+	b.dispose();
+	t.deepEqual(root.listNodes(), [], 'Disposed a node.');
+
+	// Subjective behavior, but might as well unit test it.
+	root.addNode(a).addNode(b).addNode(b).addNode(b);
+	t.deepEqual(root.listNodes(), [a, b, b, b], 'Added duplicate nodes.');
+	root.removeNode(b);
+	t.deepEqual(root.listNodes(), [a], 'Removed a duplicate node.');
+	root.removeNode(b).removeNode(b).removeNode(b);
+	t.deepEqual(root.listNodes(), [a], 'Removed a non-present node repeatedly.');
+
+	a.detach();
+	t.deepEqual(root.listNodes(), [], 'Detached a node.');
+
+	root.addNode(b);
+	root.dispose();
+	t.deepEqual(root.listNodes(), [], 'Disposed the root, confirmed empty.');
+	t.equal(root.isDisposed(), true, 'Disposed the root, confirmed disposed.');
+
+	t.end();
+});
--- packages/core/test/io.test.js
+++ packages/core/test/io.test.js
@@ -4,12 +4,52 @@ const fs = require('fs');
 const test = require('tape');
 const glob = require('glob');
 const path = require('path');
-const { GLTFUtil, NodeIO, Root, Graph, Node } = require('../');
+const { NodeIO } = require('../');
 
-// function ensureDir(uri) {
-//   const outdir = path.dirname(uri);
-//   if (!fs.existsSync(outdir)) fs.mkdirSync(outdir);
-// }
+function ensureDir(uri) {
+	const outdir = path.dirname(uri);
+	if (!fs.existsSync(outdir)) fs.mkdirSync(outdir);
+}
+
+test('@gltf-transform/core::io | read glb', t => {
+	glob.sync(path.join(__dirname, 'in', '**/*.glb')).forEach((inputURI) => {
+		const basepath = inputURI.replace(path.join(__dirname, 'in'), '');
+
+		const io = new NodeIO(fs, path);
+		const container = io.read(inputURI);
+
+		t.ok(container, `Read "${basepath}".`)
+	});
+	t.end();
+});
+
+test('@gltf-transform/core::io | read gltf', t => {
+	glob.sync(path.join(__dirname, 'in', '**/*.gltf')).forEach((inputURI) => {
+		const basepath = inputURI.replace(path.join(__dirname, 'in'), '');
+
+		const io = new NodeIO(fs, path);
+		const container = io.read(inputURI);
+
+		t.ok(container, `Read "${basepath}".`)
+	});
+	t.end();
+});
+
+test('@gltf-transform/core::io | write glb', t => {
+	glob.sync(path.join(__dirname, 'in', '**/*.gltf')).forEach((inputURI) => {
+		const basepath = inputURI.replace(path.join(__dirname, 'in'), '');
+		const outputURI = path.join(__dirname, 'out', basepath);
+
+		const io = new NodeIO(fs, path);
+		const container = io.read(inputURI);
+
+		t.ok(container, `Read "${basepath}".`);
+		ensureDir(outputURI);
+		io.write(outputURI.replace('.gltf', '.glb'), container);
+		// TODO(donmccurdy): Test the output somehow.
+	});
+	t.end();
+});
 
 // test('@gltf-transform/core::io -- glb', t => {
 //   glob.sync(path.join(__dirname, 'in', '**/*.glb')).forEach((inputURI) => {
@@ -71,62 +111,3 @@ const { GLTFUtil, NodeIO, Root, Graph, Node } = require('../');
 //   }, 'BoxVertexColors.glb -- analysis');
 //   t.end();
 // });
-
-test('@gltf-transform/core::io -- glb', t => {
-  glob.sync(path.join(__dirname, 'in', '**/*.glb')).forEach((inputURI) => {
-    const basepath = inputURI.replace(path.join(__dirname, 'in'), '');
-
-    const io = new NodeIO(fs, path);
-    const container = io.read_v2(inputURI);
-
-    t.ok(container, `Read "${basepath}".`)
-  });
-  t.end();
-});
-
-
-test('@gltf-transform/core::io -- gltf', t => {
-  glob.sync(path.join(__dirname, 'in', '**/*.gltf')).forEach((inputURI) => {
-    const basepath = inputURI.replace(path.join(__dirname, 'in'), '');
-
-    const io = new NodeIO(fs, path);
-    const container = io.read_v2(inputURI);
-
-    t.ok(container, `Read "${basepath}".`)
-  });
-  t.end();
-});
-
-test('@gltf-transform/core::graph', t => {
-  const graph = new Graph();
-  const root = new Root(graph);
-  const a = new Node(graph);
-  const b = new Node(graph);
-
-  root.addNode(a).addNode(b);
-  t.deepEqual(root.listNodes(), [a, b], 'Added two nodes.');
-
-  root.removeNode(a);
-  t.deepEqual(root.listNodes(), [b], 'Removed a node.');
-
-  b.dispose();
-  t.deepEqual(root.listNodes(), [], 'Disposed a node.');
-
-  // Subjective behavior, but might as well unit test it.
-  root.addNode(a).addNode(b).addNode(b).addNode(b);
-  t.deepEqual(root.listNodes(), [a, b, b, b], 'Added duplicate nodes.');
-  root.removeNode(b);
-  t.deepEqual(root.listNodes(), [a], 'Removed a duplicate node.');
-  root.removeNode(b).removeNode(b).removeNode(b);
-  t.deepEqual(root.listNodes(), [a], 'Removed a non-present node repeatedly.');
-
-  a.detach();
-  t.deepEqual(root.listNodes(), [], 'Detached a node.');
-
-  root.addNode(b);
-  root.dispose();
-  t.deepEqual(root.listNodes(), [], 'Disposed the root, confirmed empty.');
-  t.equal(root.isDisposed(), true, 'Disposed the root, confirmed disposed.');
-
-  t.end();
-});
