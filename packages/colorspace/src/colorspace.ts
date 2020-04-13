826790b2ddbb723abd9774cb73e796d7be5b7e2e
Don McCurdy <donmccurdy@google.com>
2020-04-12 21:59:57 -0700

    v2 - reimplement colorspace

--- packages/colorspace/src/colorspace.ts
+++ packages/colorspace/src/colorspace.ts
@@ -1,4 +1,4 @@
-import { GLTFContainer, GLTFUtil, LoggerVerbosity, AccessorTypeData } from '@gltf-transform/core';
+import { Accessor, Container, GLTFUtil, LoggerVerbosity, Primitive, Vector3 } from '@gltf-transform/core';
 
 const logger = GLTFUtil.createLogger('@gltf-transform/colorspace', LoggerVerbosity.INFO);
 
@@ -6,72 +6,56 @@ interface IColorspaceOptions {
     inputEncoding: string;
 }
 
-function colorspace (container: GLTFContainer, options: IColorspaceOptions): GLTFContainer {
+export function colorspace (container: Container, options: IColorspaceOptions): Container {
 
     if (options.inputEncoding === 'linear') {
-        logger.info(`Vertex colors already linear. Skipping conversion.`);
+        logger.info('Vertex colors already linear. Skipping conversion.');
         return container;
     }
 
     if (options.inputEncoding !== 'sRGB') {
-        logger.error(`Unknown input encoding "${options.inputEncoding}" – should be "sRGB" or "linear". Skipping conversion.`);
+		logger.error(
+			`Unknown input encoding "${options.inputEncoding}" – should be "sRGB" or "linear". `
+			+ 'Skipping conversion.'
+		);
         return container;
     }
 
-    const converted = new Set();
+    const converted = new Set<Accessor>();
 
-    container.json.meshes.forEach((mesh) => {
-        mesh.primitives.forEach((primitive) => {
-            for (let i = 0; primitive.attributes[`COLOR_${i}`] !== undefined; i++) {
+	// Source: THREE.Color
+	function sRGBToLinear( c: number ): number {
 
-                const accessorIndex = primitive.attributes[`COLOR_${i}`];
-                if (converted.has(accessorIndex)) continue;
+		return ( c < 0.04045 )
+			? c * 0.0773993808
+			: Math.pow( c * 0.9478672986 + 0.0521327014, 2.4 );
 
-                const accessorDef = container.json.accessors[accessorIndex];
-                const accessorData = container.getAccessorArray(accessorIndex);
-                const valueSize = AccessorTypeData[accessorDef.type].size;
+	}
 
-                if (accessorDef.min) {
-                    accessorDef.min[0] = accessorDef.min[1] = accessorDef.min[2] = Infinity;
-                }
-                if (accessorDef.max) {
-                    accessorDef.max[0] = accessorDef.max[1] = accessorDef.max[2] = -Infinity;
-                }
+	function updatePrimitive(primitive: Primitive): void {
+		const color = new Vector3();
+		let attribute: Accessor;
+		for (let i = 0; (attribute = primitive.getAttribute(`COLOR_${i}`)); i++) {
+			if (converted.has(attribute)) continue;
 
-                for (let j = 0; j < accessorData.length; j += valueSize) {
-                    accessorData[j + 0] = SRGBToLinear(accessorData[j + 0]);
-                    accessorData[j + 1] = SRGBToLinear(accessorData[j + 1]);
-                    accessorData[j + 2] = SRGBToLinear(accessorData[j + 2]);
+			for (let j = 0; j < attribute.getCount(); j++) {
+				attribute.getXYZ(j, color);
+				color.x = sRGBToLinear(color.x);
+				color.y = sRGBToLinear(color.y);
+				color.z = sRGBToLinear(color.z);
+				attribute.setXYZ(j, color);
+			}
 
-                    if (accessorDef.min) {
-                        accessorDef.min[0] = Math.min(accessorData[j + 0], accessorDef.min[0]);
-                        accessorDef.min[1] = Math.min(accessorData[j + 1], accessorDef.min[1]);
-                        accessorDef.min[2] = Math.min(accessorData[j + 2], accessorDef.min[2]);
-                    }
+			converted.add(attribute);
+		}
+	}
 
-                    if (accessorDef.max) {
-                        accessorDef.max[0] = Math.max(accessorData[j + 0], accessorDef.max[0]);
-                        accessorDef.max[1] = Math.max(accessorData[j + 1], accessorDef.max[1]);
-                        accessorDef.max[2] = Math.max(accessorData[j + 2], accessorDef.max[2]);
-                    }
-                }
-
-                converted.add(accessorIndex);
-            }
-        });
-    });
+	container.getRoot()
+		.listMeshes()
+		.forEach((mesh) => mesh.listPrimitives().forEach(updatePrimitive));
 
     logger.info(`Corrected vertex colors from ${options.inputEncoding} to linear.`);
 
     return container;
 
 }
-
-// Source: THREE.Color
-function SRGBToLinear( c ) {
-
-    return ( c < 0.04045 ) ? c * 0.0773993808 : Math.pow( c * 0.9478672986 + 0.0521327014, 2.4 );
-
-}
-
-export { colorspace };
--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -1,16 +1,9 @@
-import { AccessorComponentType, AccessorComponentTypeData, AccessorType, AccessorTypeData, BufferViewTarget } from './constants';
-import { Element } from './elements/element';
-import { Material, Mesh, Node, Root } from './elements/index';
-import { Graph } from './graph/index';
-import { NodeIO, WebIO } from './io/io';
-import { Logger, LoggerVerbosity } from './logger';
-import { GLTFUtil } from './util';
-
-// TODO(donmccurdy): Exports are incomplete.
-export { AccessorComponentType, AccessorComponentTypeData, AccessorType, AccessorTypeData, BufferViewTarget };
-export { Element };
-export { Material, Mesh, Node, Root };
-export { Graph };
-export { NodeIO, WebIO };
-export { Logger, LoggerVerbosity };
-export { GLTFUtil };
+export { AccessorComponentType, AccessorComponentTypeData, AccessorType, AccessorTypeData, BufferViewTarget } from './constants';
+export { Container } from './container';
+export { Element } from './elements/element';
+export { Accessor, Buffer, Material, Mesh, Node, Primitive, Root, Scene, Texture } from './elements/index';
+export { Graph } from './graph/index';
+export { NodeIO, WebIO } from './io/io';
+export { Logger, LoggerVerbosity } from './logger';
+export { GLTFUtil } from './util';
+export { Vector2, Vector3, Vector4 } from './math';
