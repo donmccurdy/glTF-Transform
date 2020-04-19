e4793f6f06e0af28d681279848d971a4d87b4823
Don McCurdy <donmccurdy@google.com>
2020-04-19 16:29:20 -0700

    Update AO package.

--- packages/ao/src/ao.ts
+++ packages/ao/src/ao.ts
@@ -1,112 +1,100 @@
-export const NOT_IMPLEMENTED = true;
+import * as geoao from 'geo-ambient-occlusion';
+import * as REGL from 'regl';
+import { Container, Logger, LoggerVerbosity, Primitive } from '@gltf-transform/core';
 
-// import { GLTFContainer, GLTFUtil, LoggerVerbosity, AccessorComponentType, BufferViewTarget } from '@gltf-transform/core';
-// import * as geoaoNamespace from 'geo-ambient-occlusion';
-// import * as reglNamespace from 'regl';
+const logger = new Logger('@gltf-transform/ao', LoggerVerbosity.INFO);
 
-// const REGL = reglNamespace['default'] as Function;
-// const geoao = geoaoNamespace['default'] as Function;
-// const logger = GLTFUtil.createLogger('@gltf-transform/ao', LoggerVerbosity.INFO);
+interface GLFactory {
+	(w: number, h: number): WebGLRenderingContext;
+}
 
-// interface GLFactory {
-//     (w: number, h: number): WebGLRenderingContext;
-// }
+interface IOcclusionOptions {
+	gl?: GLFactory;
+	resolution: number;
+	samples: number;
+}
 
-// interface IOcclusionOptions {
-//     gl?: GLFactory;
-//     resolution: number;
-//     samples: number;
-// }
+const DEFAULT_OPTIONS: IOcclusionOptions = {
+	resolution: 512,
+	samples: 500,
+};
 
-// const DEFAULT_OPTIONS: IOcclusionOptions = {
-//     resolution: 512,
-//     samples: 500,
-// };
+// A greyscale 256x1 gradient.
+const TEXTURE_MIME_TYPE = 'image/png';
+const TEXTURE_DATA = new Uint8Array([
+	137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 1, 0, 0, 0, 0, 1, 8, 6, 0,
+	0, 0, 49, 89, 112, 119, 0, 0, 0, 32, 73, 68, 65, 84, 56, 79, 99, 100, 96, 96, 248, 207, 196,
+	196, 196, 0, 2, 163, 244, 104, 56, 140, 166, 131, 145, 147, 15, 24, 25, 25, 25, 0, 254, 131,
+	3, 254, 243, 176, 75, 70, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130
+]).buffer;
 
-// // A greyscale 256x1 gradient.
-// const TEXTURE_MIME_TYPE = 'image/png';
-// const TEXTURE_DATA = new Uint8Array([
-//     137, 80, 78, 71, 13, 10, 26, 10, 0, 0, 0, 13, 73, 72, 68, 82, 0, 0, 1, 0, 0, 0, 0, 1, 8, 6, 0,
-//     0, 0, 49, 89, 112, 119, 0, 0, 0, 32, 73, 68, 65, 84, 56, 79, 99, 100, 96, 96, 248, 207, 196,
-//     196, 196, 0, 2, 163, 244, 104, 56, 140, 166, 131, 145, 147, 15, 24, 25, 25, 25, 0, 254, 131,
-//     3, 254, 243, 176, 75, 70, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130
-// ]).buffer;
+function ao (container: Container, options: IOcclusionOptions): void {
+	options = {...DEFAULT_OPTIONS, ...options};
+	const {resolution, samples} = options;
+	logger.info(`Resolution: ${resolution}; Samples: ${samples}`);
 
-// function ao (container: GLTFContainer, options: IOcclusionOptions): GLTFContainer {
-//     options = {...DEFAULT_OPTIONS, ...options};
-//     const {resolution, samples} = options;
-//     logger.info(`Resolution: ${resolution}; Samples: ${samples}`);
+	const primitives = new Set<Primitive>();
+	container.getRoot().listMeshes().forEach((mesh) => {
+		mesh.listPrimitives().forEach((primitive) => (primitives.add(primitive)));
+	});
 
-//     const primitives = [];
-//     const meshes = container.json.meshes || [];
-//     meshes.forEach((mesh) => {
-//         mesh.primitives.forEach((primitive) => {
-//             const position = container.getAccessorArray(primitive.attributes['POSITION']);
-//             const cells = primitive.indices !== undefined ? container.getAccessorArray(primitive.indices) : undefined;
-//             primitives.push({position, cells, def: primitive});
-//         })
-//     });
+	if (primitives.size === 0) {
+		logger.warn('No primitives found.');
+		return;
+	}
 
-//     if (primitives.length === 0) {
-//         logger.warn('No primitives found.');
-//         return;
-//     }
+	const texture = container.createTexture('occlusion')
+	.setImage(TEXTURE_DATA)
+	.setMimeType(TEXTURE_MIME_TYPE);
 
-//     GLTFUtil.addImage(container, 'occlusion', TEXTURE_DATA, TEXTURE_MIME_TYPE);
-//     container.json.textures.push({source: container.json.images.length - 1});
-//     const occlusionTextureIndex = container.json.textures.length - 1;
+	let regl;
+	if (options.gl) {
+		const gl = options.gl(resolution, resolution);
+		gl.getExtension('OES_texture_float');
+		gl.getExtension('OES_element_index_uint');
+		regl = REGL({gl, extensions: ['OES_texture_float', 'OES_element_index_uint']});
+	}
 
-//     let regl;
-//     if (options.gl) {
-//         const gl = options.gl(resolution, resolution);
-//         gl.getExtension('OES_texture_float');
-//         gl.getExtension('OES_element_index_uint');
-//         regl = REGL({gl, extensions: ['OES_texture_float', 'OES_element_index_uint']});
-//     }
+	// TODO: Implement baking such that primitives affect other primitives, and respect
+	// world transforms.
+	Array.from(primitives).forEach((primitive, index) => {
+		logger.info(`Baking primitive ${index} / ${primitives.size}.`);
 
-//     // TODO: Implement baking such that primitives affect other primitives, and respect
-//     // world transforms.
-//     primitives.forEach((primitive, index) => {
-//         logger.info(`Baking primitive ${index} / ${primitives.length}.`);
+		if (primitive.getMaterial().getOcclusionTexture()) {
+			// TODO: Duplicate the material if needed.
+			logger.error('Primitive already has AO. Is it sharing a material? Skipping.');
+			return;
+		}
 
-//         const primitiveDef: GLTF.IMeshPrimitive = primitive.def;
-//         if (container.json.materials[primitiveDef.material].occlusionTexture) {
-//             // TODO: Duplicate the material if needed.
-//             logger.error(`Primitive material already has AO map, and may be shared by another primitive. Skipping.`);
-//             return;
-//         }
+		// Bake vertex AO.
+		const position = primitive.getAttribute('POSITION').getArray();
+		const cells = primitive.getIndices() ? primitive.getIndices().getArray() : undefined;
+		const aoSampler = geoao(position, {cells, resolution, regl});
+		for (let i = 0; i < samples; i++) aoSampler.sample();
+		const ao = aoSampler.report();
+		aoSampler.dispose();
 
-//         // Bake vertex AO.
-//         const {position, cells} = primitive;
-//         const aoSampler = geoao(position, {cells, resolution, regl});
-//         for (let i = 0; i < samples; i++) aoSampler.sample();
-//         const ao = aoSampler.report();
-//         aoSampler.dispose();
+		// Write UV set and add AO map.
+		const numVertices = ao.length;
+		const uv2Data = new Float32Array(numVertices * 2);
+		for (let i = 0; i < numVertices; i++) {
+			uv2Data[i * 2] = uv2Data[i * 2 + 1] = 1 - ao[i];
+		}
 
-//         // Write UV set and add AO map.
-//         const numVertices = ao.length;
-//         const uv2Data = new Float32Array(numVertices * 2);
-//         for (let i = 0; i < numVertices; i++) {
-//             uv2Data[i * 2] = uv2Data[i * 2 + 1] = 1 - ao[i];
-//         }
-//         GLTFUtil.addAccessor(
-//             container,
-//             uv2Data,
-//             'VEC2' as GLTF.AccessorType.VEC2,
-//             AccessorComponentType.FLOAT,
-//             numVertices,
-//             BufferViewTarget.ARRAY_BUFFER
-//         );
-//         const accessorIndex = container.json.accessors.length - 1;
-//         primitiveDef.attributes['TEXCOORD_1'] = accessorIndex;
-//         if (primitiveDef.attributes['TEXCOORD_0'] === undefined) {
-//             primitiveDef.attributes['TEXCOORD_0'] = accessorIndex;
-//         }
-//         container.json.materials[primitiveDef.material].occlusionTexture = {index: occlusionTextureIndex};
-//     });
+		const buffer = container.getRoot().listBuffers()[0] || container.createBuffer('');
+		const uv2 = container.createAccessor('uv2', buffer)
+		.setArray(uv2Data)
+		.setType(GLTF.AccessorType.VEC2);
 
-//     logger.info('Finished baking per-vertex occlusion.');
-//     return container;
-// }
+		primitive.setAttribute('TEXCOORD_1', uv2);
+		if (!primitive.getAttribute['TEXCOORD_0']) {
+			primitive.setAttribute('TEXCOORD_0', uv2);
+		}
 
-// export { ao };
+		primitive.getMaterial().setOcclusionTexture(texture);
+	});
+
+	logger.info('Finished baking per-vertex occlusion.');
+}
+
+export { ao };
---
+++ packages/ao/test/ao.test.js
@@ -0,0 +1,32 @@
+require('source-map-support').install();
+
+const fs = require('fs');
+const path = require('path');
+const test = require('tape');
+const gl = require('gl');
+
+const { NodeIO } = require('@gltf-transform/core');
+const { ao } = require('../');
+
+
+test('@gltf-transform/ao', t => {
+	const io = new NodeIO(fs, path);
+	const container = io.read(path.join(__dirname, 'in/chr_knight.glb'));
+	const root = container.getRoot();
+	const primitive = root.listMeshes()[0].listPrimitives()[0];
+	const material = primitive.getMaterial();
+
+	t.notOk(material.getOcclusionTexture(), 'begins without occlusionTexture');
+	t.notOk(primitive.getAttribute('TEXCOORD_1'), 'begins without TEXCOORD_1');
+	t.equals(root.listTextures().length, 1, 'begins with one texture')
+
+	ao(container, {gl});
+
+	t.ok(material.getOcclusionTexture(), 'adds occlusionTexture');
+	t.ok(primitive.getAttribute('TEXCOORD_1'), 'adds TEXCOORD_1');
+	t.equals(root.listTextures().length, 2, 'adds texture')
+
+	io.write(path.join(__dirname, 'knight.gltf'), container);
+
+	t.end();
+});
