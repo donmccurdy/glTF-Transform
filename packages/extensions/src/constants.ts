03bc480702187785df6ff041afb4eefd33064439
Don McCurdy <donmccurdy@google.com>
2020-06-11 19:29:20 -0700

    [core] Add missing files.

---
+++ packages/extensions/src/constants.ts
@@ -0,0 +1,2 @@
+export const KHR_MATERIALS_UNLIT = 'KHR_materials_unlit';
+export const KHR_MESH_QUANTIZATION = 'KHR_mesh_quantization';
---
+++ packages/extensions/src/khr-materials-unlit/unlit.ts
@@ -0,0 +1,8 @@
+import { ExtensionProperty } from '@gltf-transform/core';
+import { KHR_MATERIALS_UNLIT } from '../constants';
+
+export class Unlit extends ExtensionProperty {
+	public readonly propertyType = 'Unlit';
+	public readonly extensionName = KHR_MATERIALS_UNLIT;
+	public static EXTENSION_NAME = KHR_MATERIALS_UNLIT;
+}
