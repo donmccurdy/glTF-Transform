9e807c367ed27239c5f37bb886543362a9923949
Don McCurdy <donmccurdy@google.com>
2020-07-17 18:07:25 -0700

    [core] Add node.test.js, root.test.js, and scene.test.js

---
+++ packages/core/test/properties/node.test.js
@@ -0,0 +1,31 @@
+require('source-map-support').install();
+
+const test = require('tape');
+const { Document } = require('../../');
+
+test('@gltf-transform/core::node | copy', t => {
+	const doc = new Document();
+	const node = doc.createNode('MyNode')
+		.setTranslation([1, 2, 3])
+		.setRotation([1, 0, 1, 0])
+		.setScale([2, 2, 2])
+		.setWeights([1.5, 1.5])
+		.setCamera(doc.createCamera())
+		.setMesh(doc.createMesh())
+		.setSkin(doc.createSkin())
+		.addChild(doc.createNode('OtherNode'));
+
+	const node2 = doc.createNode().copy(node);
+	t.equals(node2.getName(), 'MyNode', 'copy name');
+	t.deepEqual(node2.getTranslation(), [1, 2, 3], 'copy translation');
+	t.deepEqual(node2.getRotation(), [1, 0, 1, 0], 'copy rotation');
+	t.deepEqual(node2.getScale(), [2, 2, 2], 'copy scale');
+	t.deepEqual(node2.getWeights(), [1.5, 1.5], 'copy weights');
+
+	t.equals(node2.getCamera(), node.getCamera(), 'copy camera');
+	t.equals(node2.getMesh(), node.getMesh(), 'copy mesh');
+	t.equals(node2.getSkin(), node.getSkin(), 'copy skin');
+	t.equals(node2.listChildren()[0], node.listChildren()[0], 'copy children');
+
+	t.end();
+});
