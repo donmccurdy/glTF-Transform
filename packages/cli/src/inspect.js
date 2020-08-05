3492833dd6294aad570a0b6773a2483e7102e8c2
Don McCurdy <donmccurdy@google.com>
2020-08-05 14:50:05 -0700

    [core] Support I/O reading to NativeDocument directly.
    [cli] Ensure that 'inspect' can give basic info even about unsupported models.

--- packages/cli/src/inspect.js
+++ packages/cli/src/inspect.js
@@ -2,20 +2,31 @@ const Table = require('cli-table');
 const { inspect: inspectDoc } = require('@gltf-transform/lib');
 const { formatBytes, formatHeader, formatLong, formatParagraph } = require('./util');
 
-function inspect (doc) {
-	const logger = doc.getLogger();
-
-	// TODO(feature): Should really be able to get at least this far without parsing.
+function inspect (nativeDoc, io, logger) {
+	// Summary (does not require parsing).
+	const extensionsUsed = nativeDoc.json.extensionsUsed || [];
+	const extensionsRequired = nativeDoc.json.extensionsRequired || [];
 	const table = new Table();
 	table.push(
-		{generator: doc.getRoot().getAsset().generator},
-		{version: doc.getRoot().getAsset().version},
-		{extensionsUsed: doc.getRoot().listExtensionsUsed().join(', ') || 'none'},
-		{extensionsRequired: doc.getRoot().listExtensionsRequired().join(', ') || 'none'},
+		{generator: nativeDoc.json.asset.generator},
+		{version: nativeDoc.json.asset.version},
+		{extensionsUsed: extensionsUsed.join(', ') || 'none'},
+		{extensionsRequired: extensionsRequired.join(', ') || 'none'},
 	);
 	console.log(formatHeader('info'));
 	console.log(table.toString() + '\n');
 
+	// Parse.
+	let doc;
+	try {
+		doc = io.createDocument(nativeDoc);
+	} catch (e) {
+		logger.warn('Unable to parse document.');
+		logger.error(e.message);
+		return;
+	}
+
+	// Detailed report.
 	const report = inspectDoc(doc);
 	for (const type in report) {
 		const properties = report[type].properties;
@@ -48,6 +59,8 @@ function formatPropertyReport(property, index) {
 			row[key] = value > 0 ? formatBytes(value) : '';
 		} else if (typeof value === 'number') {
 			row[key] = formatLong(value);
+		} else if (typeof value === 'boolean') {
+			row[key] = value ? '✓' : '';
 		} else {
 			row[key] = value;
 		}
