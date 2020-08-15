74d7574c46e1e0b8c149d5a5d61de679513407d2
Don McCurdy <donmccurdy@google.com>
2020-08-14 22:59:10 -0700

    [extensions] Add tests for KHR_materials_transmission.

---
+++ packages/extensions/test/materials-transmission.test.js
@@ -0,0 +1,63 @@
+require('source-map-support').install();
+
+const fs = require('fs');
+const path = require('path');
+const test = require('tape');
+const { Document, NodeIO } = require('@gltf-transform/core');
+const { MaterialsTransmission, Transmission } = require('../');
+
+const WRITER_OPTIONS = {basename: 'extensionTest'};
+
+test('@gltf-transform/extensions::materials-transmission', t => {
+	const doc = new Document();
+	const transmissionExtension = doc.createExtension(MaterialsTransmission);
+	const transmission = transmissionExtension.createTransmission()
+		.setTransmissionFactor(0.9)
+		.setTransmissionTexture(doc.createTexture());
+
+	const mat = doc.createMaterial('MyTransmissionMaterial')
+		.setBaseColorFactor([1.0, 0.5, 0.5, 1.0])
+		.setExtension(Transmission, transmission);
+
+	t.equal(mat.getExtension(Transmission), transmission, 'transmission is attached');
+
+	const nativeDoc = new NodeIO(fs, path).createNativeDocument(doc, WRITER_OPTIONS);
+	const materialDef = nativeDoc.json.materials[0];
+
+	t.deepEqual(materialDef.pbrMetallicRoughness.baseColorFactor, [1.0, 0.5, 0.5, 1.0], 'writes base color');
+	t.deepEqual(materialDef.extensions, {KHR_materials_transmission: {
+		transmissionFactor: 0.9,
+		transmissionTexture: {index: 0, texCoord: 0},
+	}}, 'writes transmission extension');
+	t.deepEqual(nativeDoc.json.extensionsUsed, [MaterialsTransmission.EXTENSION_NAME], 'writes extensionsUsed');
+
+	transmissionExtension.dispose();
+	t.equal(mat.getExtension(Transmission), null, 'transmission is detached');
+
+	const roundtripDoc = new NodeIO(fs, path)
+		.registerExtensions([MaterialsTransmission])
+		.createDocument(nativeDoc);
+	const roundtripMat = roundtripDoc.getRoot().listMaterials().pop();
+
+	t.equal(roundtripMat.getExtension(Transmission).getTransmissionFactor(), 0.9, 'reads transmissionFactor');
+	t.ok(roundtripMat.getExtension(Transmission).getTransmissionTexture(), 'reads transmissionTexture');
+	t.end();
+});
+
+test('@gltf-transform/extensions::materials-transmission | copy', t => {
+	const doc = new Document();
+	const transmissionExtension = doc.createExtension(MaterialsTransmission);
+	const transmission = transmissionExtension.createTransmission()
+		.setTransmissionFactor(0.9)
+		.setTransmissionTexture(doc.createTexture('trns'));
+	doc.createMaterial()
+		.setExtension(Transmission, transmission);
+
+	const doc2 = doc.clone();
+	const transmission2 = doc2.getRoot().listMaterials()[0].getExtension(Transmission);
+	t.equals(doc2.getRoot().listExtensionsUsed().length, 1, 'copy MaterialsTransmission');
+	t.ok(transmission2, 'copy Transmission');
+	t.equals(transmission2.getTransmissionFactor(), 0.9, 'copy transmissionFactor');
+	t.equals(transmission2.getTransmissionTexture().getName(), 'trns', 'copy transmissionTexture');
+	t.end();
+});
