e67d705cfd0ddc00c9edbfb54bed19320895f8d5
Don McCurdy <donmccurdy@google.com>
2020-08-30 22:00:08 -0700

    [docs] ColorUtils docs.

--- packages/core/src/utils/color-utils.ts
+++ packages/core/src/utils/color-utils.ts
@@ -5,10 +5,25 @@ import { vec3, vec4 } from '../constants';
  *
  * *Common utilities for working with colors in vec3, vec4, or hexadecimal form.*
  *
+ * Provides methods to convert linear components (vec3, vec4) to sRGB hex values. All colors in
+ * the glTF specification, excluding color textures, are linear. Hexadecimal values, in sRGB
+ * colorspace, are accessible through helper functions in the API as a convenience.
+ *
+ * ```typescript
+ * // Hex (sRGB) to factor (linear).
+ * const factor = ColorUtils.hexToFactor(0xFFCCCC, []);
+ *
+ * // Factor (linear) to hex (sRGB).
+ * const hex = ColorUtils.factorToHex([1, .25, .25])
+ * ```
+ *
  * @category Utilities
  */
 export class ColorUtils {
-	/** Converts sRGB hexadecimal to linear components. */
+	/**
+	 * Converts sRGB hexadecimal to linear components.
+	 * @typeParam T vec3 or vec4 linear components.
+	 */
 	static hexToFactor<T = vec3 | vec4>(hex: number, target: T): T {
 		hex = Math.floor( hex );
 		target[0] = ( hex >> 16 & 255 ) / 255;
@@ -17,14 +32,20 @@ export class ColorUtils {
 		return this.convertSRGBToLinear<T>(target, target);
 	}
 
-	/** Converts linear components to sRGB hexadecimal. */
+	/**
+	 * Converts linear components to sRGB hexadecimal.
+	 * @typeParam T vec3 or vec4 linear components.
+	 */
 	static factorToHex<T = vec3 | vec4>(factor: T): number {
 		const target = [...factor as unknown as number[]] as unknown as T;
 		const [r, g, b] = this.convertLinearToSRGB(factor, target) as unknown as number[];
 		return ( r * 255 ) << 16 ^ ( g * 255 ) << 8 ^ ( b * 255 ) << 0;
 	}
 
-	/** Converts sRGB components to linear components. */
+	/**
+	 * Converts sRGB components to linear components.
+	 * @typeParam T vec3 or vec4 linear components.
+	 */
 	static convertSRGBToLinear<T = vec3 | vec4>(source: T, target: T): T {
 		for (let i = 0 ; i < 3; i++) {
 			target[i] = ( source[i] < 0.04045 )
@@ -34,7 +55,10 @@ export class ColorUtils {
 		return target;
 	}
 
-	/** Converts linear components to sRGB components. */
+	/**
+	 * Converts linear components to sRGB components.
+	 * @typeParam T vec3 or vec4 linear components.
+	 */
 	static convertLinearToSRGB<T = vec3 | vec4>(source: T, target: T): T {
 		for (let i = 0; i < 3; i++) {
 			target[i] = ( source[i] < 0.0031308 )
