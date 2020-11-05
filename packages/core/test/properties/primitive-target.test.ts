5db90076cfcc86956ff59b66a4ed29333840238a
Don McCurdy <donmccurdy@google.com>
2020-11-05 00:12:48 -0800

    [core] Test coverage

---
+++ packages/core/test/properties/primitive-target.test.ts
@@ -0,0 +1,23 @@
+require('source-map-support').install();
+
+import * as test from 'tape';
+import { Document, Property } from '../../';
+
+const toType = (p: Property): string => p.propertyType;
+
+test('@gltf-transform/core::primitive-target', t => {
+	const doc = new Document();
+	const prim1 = doc.createPrimitiveTarget();
+	const acc1 = doc.createAccessor('acc1');
+	prim1.setAttribute('POSITION', acc1);
+	const prim2 = prim1.clone();
+
+	t.equals(prim1.getAttribute('POSITION'), acc1, 'sets POSITION');
+	t.equals(prim2.getAttribute('POSITION'), acc1, 'sets POSITION');
+	t.deepEqual(acc1.listParents().map(toType), ['Root', 'PrimitiveTarget', 'PrimitiveTarget'], 'links POSITION');
+
+	prim1.setAttribute('POSITION', null);
+	t.equals(prim1.getAttribute('POSITION'), null, 'unsets POSITION');
+	t.deepEqual(acc1.listParents().map(toType), ['Root', 'PrimitiveTarget'], 'unlinks POSITION');
+	t.end();
+});
