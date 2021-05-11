9d381f3794d668b6990196546788c3eec64e1f3b
Don McCurdy <donmccurdy@google.com>
2021-05-10 23:00:43 -0700

    I/O: Support input/output of GLB without BIN chunk. (fixes #228)

--- packages/core/test/io/platform-io.test.ts
+++ packages/core/test/io/platform-io.test.ts
@@ -1,12 +1,33 @@
 require('source-map-support').install();
 
 import test from 'tape';
-import { NodeIO } from '../../';
+import { Document, Format, NodeIO } from '../../';
 
 test('@gltf-transform/core::io | common', t => {
 	t.throws(() => new NodeIO().readJSON({
 		json: {asset: {version: '1.0'}},
 		resources: {},
 	}), '1.0');
+	t.end();
+});
+
+test('@gltf-transform/core::io | glb without buffer', t => {
+	const doc = new Document();
+	doc.createScene().addChild(doc.createNode('MyNode'));
+
+	const io = new NodeIO();
+	const json = io.writeJSON(doc, {format: Format.GLB});
+	const binary = io.writeBinary(doc);
+
+	t.ok(json, 'writes json');
+	t.ok(binary, 'writes binary');
+	t.equals(Object.values(json.resources).length, 0, 'no buffers');
+	t.ok(io.readJSON(json), 'reads json');
+	t.ok(io.readBinary(binary), 'reads binary');
+	t.deepEquals(
+		io.readBinary(binary).getRoot().listNodes().map((n) => n.getName()),
+		['MyNode'],
+		'same nodes',
+	);
 	t.end();
 });
