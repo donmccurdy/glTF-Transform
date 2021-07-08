23ae5959faf25d7600b98f9447cef140aba0939c
Don McCurdy <donmccurdy@google.com>
2021-07-07 21:50:45 -0700

    Export 'VERSION'

--- packages/core/src/core.ts
+++ packages/core/src/core.ts
@@ -7,5 +7,5 @@ export { Accessor, Animation, AnimationChannel, AnimationSampler, Buffer, Camera
 export { Graph, GraphChild, GraphChildList, Link } from './graph/';
 export { PlatformIO, NodeIO, WebIO, ReaderContext, WriterContext } from './io/';
 export { BufferUtils, ColorUtils, FileUtils, ImageUtils, ImageUtilsFormat, Logger, MathUtils, bounds, uuid } from './utils/';
-export { TypedArray, TypedArrayConstructor, PropertyType, Format, TextureChannel, VertexLayout, vec2, vec3, vec4, mat3, mat4, bbox, GLB_BUFFER } from './constants';
+export { TypedArray, TypedArrayConstructor, PropertyType, Format, TextureChannel, VertexLayout, vec2, vec3, vec4, mat3, mat4, bbox, GLB_BUFFER, VERSION } from './constants';
 export { GLTF } from './types/gltf';
