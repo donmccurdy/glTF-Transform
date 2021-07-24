e5773f4bdb81d4e75f5f1d7169263f2e7ba318b9
Don McCurdy <don.r.mccurdy@gmail.com>
2021-07-23 22:48:17 -0700

    Clean up.

--- packages/extensions/test/meshopt-compression.test.ts
+++ packages/extensions/test/meshopt-compression.test.ts
@@ -10,13 +10,13 @@ import { MeshoptCompression, MeshQuantization } from '../';
 const MeshoptDecoder = require('../vendor/meshopt_decoder.js');
 
 test('@gltf-transform/extensions::draco-mesh-compression | decoding', async t => {
-    await MeshoptDecoder.ready;
+	await MeshoptDecoder.ready;
 
 	const io = new NodeIO()
-        .registerExtensions([MeshoptCompression, MeshQuantization])
-        .registerDependencies({'meshopt.decoder': MeshoptDecoder});
+		.registerExtensions([MeshoptCompression, MeshQuantization])
+		.registerDependencies({'meshopt.decoder': MeshoptDecoder});
 
-    const doc = io.read(path.join(__dirname, 'in', 'BoxMeshopt.glb'));
+	const doc = io.read(path.join(__dirname, 'in', 'BoxMeshopt.glb'));
 	const bbox = bounds(doc.getRoot().listScenes()[0]);
 	t.deepEquals(bbox.min.map(v => +v.toFixed(3)), [-0.5, -0.5, -0.5], 'decompress (min)');
 	t.deepEquals(bbox.max.map(v => +v.toFixed(3)), [0.5, 0.5, 0.5], 'decompress (max)');
