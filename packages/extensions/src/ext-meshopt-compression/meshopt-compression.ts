06b9e9509e7101db789a7c7b514c36e5913bfc05
Don McCurdy <don.r.mccurdy@gmail.com>
2021-07-23 21:24:21 -0700

    [extensions] Clean up fallback buffers after parsing EXT_meshopt_compression.

--- packages/extensions/src/ext-meshopt-compression/meshopt-compression.ts
+++ packages/extensions/src/ext-meshopt-compression/meshopt-compression.ts
@@ -1,4 +1,4 @@
-import { Extension, GLB_BUFFER, PropertyType, ReaderContext, WriterContext } from '@gltf-transform/core';
+import { Accessor, Buffer, Extension, GLB_BUFFER, GLTF, PropertyType, ReaderContext, WriterContext } from '@gltf-transform/core';
 import { EXT_MESHOPT_COMPRESSION } from '../constants';
 import { MeshoptDecoder } from '../../vendor/meshopt_decoder.module';
 
@@ -15,6 +15,10 @@ enum MeshoptFilter {
 	OCTAHEDRAL = 'OCTAHEDRAL',
 	QUATERNION = 'QUATERNION',
 	EXPONENTIAL = 'EXPONENTIAL',
+}
+
+interface MeshoptBufferExtension {
+	fallback?: boolean;
 }
 
 interface MeshoptBufferViewExtension {
@@ -39,11 +43,13 @@ interface MeshoptBufferViewExtension {
  */
 export class MeshoptCompression extends Extension {
 	public readonly extensionName = NAME;
-	public readonly prereadTypes = [PropertyType.BUFFER];
+	public readonly prereadTypes = [PropertyType.BUFFER, PropertyType.PRIMITIVE];
 
 	public static readonly EXTENSION_NAME = NAME;
 
-	public preread(context: ReaderContext): this {
+	private _fallbackBufferMap = new Map<Buffer, Buffer>();
+
+	public preread(context: ReaderContext, propertyType: PropertyType): this {
 		if (!MeshoptDecoder.supported) {
 			if (!this.isRequired()) return this;
 			throw new Error(`[${NAME}]: Missing WASM support.`);
@@ -54,9 +60,19 @@ export class MeshoptCompression extends Extension {
 			throw new Error(`[${NAME}]: Decoder not ready.`);
 		}
 
+		if (propertyType === PropertyType.BUFFER) {
+			this._beforeBuffers(context);
+		} else if (propertyType === PropertyType.PRIMITIVE) {
+			this._beforePrimitives(context);
+		}
+
+		return this;
+	}
+
+	/** Decode buffer views. */
+	private _beforeBuffers(context: ReaderContext): void {
 		const jsonDoc = context.jsonDoc;
 
-		// Decode buffer views.
 		const viewDefs = jsonDoc.json.bufferViews || [];
 		viewDefs.forEach((viewDef, index) => {
 			if (!viewDef.extensions || !viewDef.extensions[NAME]) return;
@@ -80,12 +96,49 @@ export class MeshoptCompression extends Extension {
 
 			context.bufferViews[index] = result;
 		});
+	}
 
-		return this;
+	/**
+	 * Mark fallback buffers and replacements.
+	 *
+	 * Note: Alignment with primitives is arbitrary; this just needs to happen
+	 * after Buffers have been parsed.
+	 */
+	private _beforePrimitives(context: ReaderContext): void {
+		const jsonDoc = context.jsonDoc;
+		const viewDefs = jsonDoc.json.bufferViews || [];
+
+		//
+		viewDefs.forEach((viewDef) => {
+			if (!viewDef.extensions || !viewDef.extensions[NAME]) return;
+
+			const meshoptDef = viewDef.extensions[NAME] as MeshoptBufferViewExtension;
+
+			const buffer = context.buffers[meshoptDef.buffer];
+			const fallbackBuffer = context.buffers[viewDef.buffer];
+			const fallbackBufferDef = jsonDoc.json.buffers![viewDef.buffer];
+			if (isFallbackBuffer(fallbackBufferDef)) {
+				this._fallbackBufferMap.set(fallbackBuffer, buffer);
+			}
+		});
 	}
 
 	public read(_context: ReaderContext): this {
-		this.dispose(); // Writes aren't implemented, so remove extension after unpacking.
+		// Writes aren't implemented, so remove extension after unpacking.
+		this.dispose();
+
+		if (!this.isRequired()) return this;
+
+		// Replace fallback buffers.
+		for (const [fallbackBuffer, buffer] of this._fallbackBufferMap) {
+			for (const parent of fallbackBuffer.listParents()) {
+				if (parent instanceof Accessor) {
+					parent.swap(fallbackBuffer, buffer);
+				}
+			}
+			fallbackBuffer.dispose();
+		}
+
 		return this;
 	}
 
@@ -93,4 +146,18 @@ export class MeshoptCompression extends Extension {
 		this.doc.getLogger().warn(`Writing ${this.extensionName} not yet implemented.`);
 		return this;
 	}
+}
+
+/**
+ * Returns true for a fallback buffer, else false.
+ *
+ *   - All references to the fallback buffer must come from bufferViews that
+ *     have a EXT_meshopt_compression extension specified.
+ *   - No references to the fallback buffer may come from
+ *     EXT_meshopt_compression extension JSON.
+ */
+function isFallbackBuffer(bufferDef: GLTF.IBuffer): boolean {
+	if (!bufferDef.extensions || !bufferDef.extensions[NAME]) return false;
+	const fallbackDef = bufferDef.extensions[NAME] as MeshoptBufferExtension;
+	return !!fallbackDef.fallback;
 }
