4d071f5e0e57e493900bfbc30fe6291f2c61551b
Don McCurdy <don.r.mccurdy@gmail.com>
2021-09-14 18:45:16 -0700

    Add failing test for texture-only buffer.

--- packages/core/test/io/platform-io.test.ts
+++ packages/core/test/io/platform-io.test.ts
@@ -31,3 +31,37 @@ test('@gltf-transform/core::io | glb without buffer', t => {
 	);
 	t.end();
 });
+
+test('@gltf-transform/core::io | glb with texture-only buffer', t => {
+	const doc = new Document();
+	const texA = doc.createTexture('TexA').setImage(new ArrayBuffer(1)).setMimeType('image/png');
+	const texB = doc.createTexture('TexB').setImage(new ArrayBuffer(2)).setMimeType('image/png');
+	doc.createMaterial('MaterialA').setBaseColorTexture(texA);
+	doc.createMaterial('MaterialB').setBaseColorTexture(texB);
+
+	const io = new NodeIO();
+
+	t.throws(() => io.writeJSON(doc, {format: Format.GLB}), 'no writeJSON without buffer');
+	t.throws(() => io.writeBinary(doc), 'no writeBinary without buffer');
+
+	doc.createBuffer();
+
+	const json = io.writeJSON(doc, {format: Format.GLB});
+	const binary = io.writeBinary(doc);
+
+	t.ok(json, 'writes json');
+	t.ok(binary, 'writes binary');
+	t.equals(Object.values(json.resources).length, 1, 'writes 1 buffer');
+	t.ok(io.readJSON(json), 'reads json');
+	t.ok(io.readBinary(binary), 'reads binary');
+
+	const rtTextures = io.readBinary(binary).getRoot()
+		.listMaterials()
+		.map((n) => n.getBaseColorTexture());
+
+	t.equals(rtTextures[0].getName(), 'TexA', 'reads texture 1');
+	t.equals(rtTextures[1].getName(), 'TexB', 'reads texture 1');
+	t.deepEquals(rtTextures[0].getImage(), new ArrayBuffer(1), 'reads texture 1 data');
+	t.deepEquals(rtTextures[1].getImage(), new ArrayBuffer(2), 'reads texture 2 data');
+	t.end();
+});
