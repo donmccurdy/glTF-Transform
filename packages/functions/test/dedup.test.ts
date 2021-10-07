e59b74b9a0c78cb39b69095639fa22ab8378c079
Don McCurdy <don.r.mccurdy@gmail.com>
2021-10-06 21:53:26 -0700

    [functions] Test for dedup animation samplers.

--- packages/functions/test/dedup.test.ts
+++ packages/functions/test/dedup.test.ts
@@ -22,6 +22,39 @@ test('@gltf-transform/functions::dedup | accessors', t => {
 	t.end();
 });
 
+
+test('@gltf-transform/functions::dedup | animation accessors', t => {
+	const doc = new Document();
+	const a = doc.createAccessor().setArray(new Float32Array([1, 2, 3]));
+	const b = doc.createAccessor().setArray(new Float32Array([4, 5, 6]));
+	const sampler1 = doc.createAnimationSampler().setInput(a).setOutput(b);
+	const sampler2 = doc.createAnimationSampler().setInput(a.clone()).setOutput(b.clone());
+	const sampler3 = doc.createAnimationSampler().setInput(a.clone()).setOutput(a.clone());
+	const prim = doc.createPrimitive().setAttribute('POSITION', a.clone());
+	doc.createMesh().addPrimitive(prim);
+	doc.createAnimation().addSampler(sampler1).addSampler(sampler2).addSampler(sampler3);
+
+	t.equal(doc.getRoot().listAccessors().length, 7, 'begins with duplicate accessors');
+
+	dedup({propertyTypes: [PropertyType.TEXTURE]})(doc);
+
+	t.equal(doc.getRoot().listAccessors().length, 7, 'has no effect when disabled');
+
+	dedup()(doc);
+
+	t.equal(doc.getRoot().listAccessors().length, 4, 'prunes duplicate accessors');
+	t.ok(sampler1.getInput() === a, 'sampler 1 input');
+	t.ok(sampler1.getOutput() === b, 'sampler 1 output');
+	t.ok(sampler2.getInput() === a, 'sampler 2 input');
+	t.ok(sampler2.getOutput() === b, 'sampler 2 output');
+	t.ok(sampler3.getInput() === a, 'sampler 3 input');
+	t.ok(sampler3.getOutput() !== b, 'no mixing input/output');
+	t.ok(sampler3.getOutput() !== b, 'no mixing input/output');
+	t.ok(prim.getAttribute('POSITION') !== a, 'no mixing sampler/attribute');
+	t.ok(prim.getAttribute('POSITION') !== b, 'no mixing sampler/attribute');
+	t.end();
+});
+
 test('@gltf-transform/functions::dedup | meshes', t => {
 	const io = new NodeIO();
 	const doc = io.read(path.join(__dirname, 'in/many-cubes.gltf'));
