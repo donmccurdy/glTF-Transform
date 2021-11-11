db422d0d51ba783bb44ab46ae8c6491d9b9722b2
Don McCurdy <dm@donmccurdy.com>
2021-11-10 18:29:51 -0800

    Clean up.

--- packages/core/test/extension.test.ts
+++ packages/core/test/extension.test.ts
@@ -120,7 +120,7 @@ test('@gltf-transform/core::extension | i/o', (t) => {
 	// Write (registered).
 
 	jsonDoc = io.writeJSON(doc, options);
-	t.deepEqual(jsonDoc.json.extensionsUsed, ['TEST_node_gizmo'], 'write extensionsUsed (registered)yar');
+	t.deepEqual(jsonDoc.json.extensionsUsed, ['TEST_node_gizmo'], 'write extensionsUsed (registered)');
 	t.equal(jsonDoc.json.extensionsRequired, undefined, 'omit extensionsRequired');
 	t.equal(jsonDoc.json.nodes[0].extensions.TEST_node_gizmo.isGizmo, true, 'extend node');
 
