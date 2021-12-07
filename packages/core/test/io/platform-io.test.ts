12a15933f23cf97e89ed6c0202a7a5dddb5f4126
Don McCurdy <dm@donmccurdy.com>
2021-12-07 14:54:07 -0800

    [core] Support reading GLB with Data URIs. (#449)

--- packages/core/test/io/platform-io.test.ts
+++ packages/core/test/io/platform-io.test.ts
@@ -3,7 +3,7 @@ require('source-map-support').install();
 import test from 'tape';
 import fs from 'fs';
 import path from 'path';
-import { Document, Format, JSONDocument, NodeIO } from '../../';
+import { BufferUtils, Document, Format, GLB_BUFFER, JSONDocument, NodeIO } from '../../';
 
 test('@gltf-transform/core::io | common', (t) => {
 	t.throws(
@@ -91,6 +91,52 @@ test('@gltf-transform/core::io | glb with texture-only buffer', (t) => {
 	t.equals(rtTextures[1].getName(), 'TexB', 'reads texture 1');
 	t.deepEquals(rtTextures[0].getImage(), new Uint8Array(1), 'reads texture 1 data');
 	t.deepEquals(rtTextures[1].getImage(), new Uint8Array(2), 'reads texture 2 data');
+	t.end();
+});
+
+test('@gltf-transform/core::io | glb with data uri', (t) => {
+	const doc = new Document();
+	doc.createTexture('TexA').setImage(new Uint8Array(1)).setMimeType('image/png');
+	doc.createTexture('TexB').setImage(new Uint8Array(2)).setMimeType('image/png');
+	doc.createBuffer();
+
+	// (1) Write JSONDocument and replace resources with Data URIs.
+
+	const io = new NodeIO();
+	const { json, resources } = io.writeJSON(doc, { format: Format.GLB });
+	for (const buffer of json.buffers!) {
+		const uri = buffer.uri || GLB_BUFFER;
+		const resource = resources[uri]!;
+		buffer.uri = `data:text/plain;base64,` + Buffer.from(resource).toString('base64');
+		delete resources[uri];
+	}
+
+	t.ok(json, 'writes json');
+	t.ok(json.buffers[0].uri.startsWith('data:'), 'buffer contains data uri');
+	t.equals(Object.values(resources).length, 0, 'no external resources');
+
+	// (2) Convert the JSONDocument to a GLB without a BIN chunk.
+
+	const header = new Uint32Array([0x46546c67, 2, 12]);
+
+	const jsonText = JSON.stringify(json);
+	const jsonChunkData = BufferUtils.pad(BufferUtils.encodeText(jsonText), 0x20);
+	const jsonChunkHeader = BufferUtils.toView(new Uint32Array([jsonChunkData.byteLength, 0x4e4f534a]));
+	const jsonChunk = BufferUtils.concat([jsonChunkHeader, jsonChunkData]);
+	header[header.length - 1] += jsonChunk.byteLength;
+
+	const binary = BufferUtils.concat([BufferUtils.toView(header), jsonChunk]);
+
+	t.ok(binary, 'writes binary');
+
+	// (3) Test that we can read GLBs with Data URIs.
+
+	const rtTextures = io.readBinary(binary).getRoot().listTextures();
+
+	t.equals(rtTextures[0].getName(), 'TexA', 'reads texture 1');
+	t.equals(rtTextures[1].getName(), 'TexB', 'reads texture 1');
+	t.deepEquals(Array.from(rtTextures[0].getImage()), Array.from(new Uint8Array(1)), 'reads texture 1 data');
+	t.deepEquals(Array.from(rtTextures[1].getImage()), Array.from(new Uint8Array(2)), 'reads texture 2 data');
 	t.end();
 });
 
