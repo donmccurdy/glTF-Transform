07ab946c8fad60ec07744746703a13c41ca455d8
Don McCurdy <dm@donmccurdy.com>
2022-02-10 21:46:18 -0500

    [cli] Ensure consistent merge order.

--- packages/cli/src/transforms/merge.ts
+++ packages/cli/src/transforms/merge.ts
@@ -15,24 +15,24 @@ const merge = (options: MergeOptions): Transform => {
 	return async (doc: Document): Promise<void> => {
 		const logger = doc.getLogger();
 
-		await Promise.all(
-			paths.map(async (path, index) => {
-				logger.debug(`Merging ${index + 1} / ${paths.length}, ${path}`);
+		for (let i = 0; i < paths.length; i++) {
+			const path = paths[i];
 
-				const basename = FileUtils.basename(path);
-				const extension = FileUtils.extension(path).toLowerCase();
-				if (['png', 'jpg', 'jpeg', 'webp', 'ktx2'].includes(extension)) {
-					doc.createTexture(basename)
-						.setImage(fs.readFileSync(path))
-						.setMimeType(ImageUtils.extensionToMimeType(extension))
-						.setURI(basename + '.' + extension);
-				} else if (['gltf', 'glb'].includes(extension)) {
-					doc.merge(await io.read(path));
-				} else {
-					throw new Error(`Unknown file extension: "${extension}".`);
-				}
-			})
-		);
+			logger.debug(`Merging ${i + 1} / ${paths.length}, ${path}`);
+
+			const basename = FileUtils.basename(path);
+			const extension = FileUtils.extension(path).toLowerCase();
+			if (['png', 'jpg', 'jpeg', 'webp', 'ktx2'].includes(extension)) {
+				doc.createTexture(basename)
+					.setImage(fs.readFileSync(path))
+					.setMimeType(ImageUtils.extensionToMimeType(extension))
+					.setURI(basename + '.' + extension);
+			} else if (['gltf', 'glb'].includes(extension)) {
+				doc.merge(await io.read(path));
+			} else {
+				throw new Error(`Unknown file extension: "${extension}".`);
+			}
+		}
 
 		if (!options.partition) {
 			const buffer = doc.getRoot().listBuffers()[0];
