63c2faef5a92191922e521ad3658a24ce7d3aac4
Don McCurdy <dm@donmccurdy.com>
2022-04-03 15:41:04 -0400

    Exclude root->texture refs from inspect() report
    Fixes https://github.com/donmccurdy/glTF-Report-Feedback/issues/22

--- packages/functions/src/inspect.ts
+++ packages/functions/src/inspect.ts
@@ -1,8 +1,18 @@
-import { Accessor, Document, ExtensionProperty, GLTF, ImageUtils, Texture, TypedArray, bounds } from '@gltf-transform/core';
+import {
+	Accessor,
+	Document,
+	ExtensionProperty,
+	GLTF,
+	ImageUtils,
+	Texture,
+	TypedArray,
+	bounds,
+	PropertyType,
+} from '@gltf-transform/core';
 import { getGLPrimitiveCount } from './utils';
 
 /** Inspects the contents of a glTF file and returns a JSON report. */
-export function inspect (doc: Document): InspectReport {
+export function inspect(doc: Document): InspectReport {
 	return {
 		scenes: listScenes(doc),
 		meshes: listMeshes(doc),
@@ -13,176 +23,190 @@ export function inspect (doc: Document): InspectReport {
 }
 
 /** List scenes. */
-function listScenes (doc: Document): InspectPropertyReport<InspectSceneReport> {
-	const scenes = doc.getRoot().listScenes().map((scene) => {
-		const root = scene.listChildren()[0];
-		const sceneBounds = bounds(scene);
-		return {
-			name: scene.getName(),
-			rootName: root ? root.getName() : '',
-			bboxMin: toPrecision(sceneBounds.min),
-			bboxMax: toPrecision(sceneBounds.max),
-		};
-	});
-	return {properties: scenes};
+function listScenes(doc: Document): InspectPropertyReport<InspectSceneReport> {
+	const scenes = doc
+		.getRoot()
+		.listScenes()
+		.map((scene) => {
+			const root = scene.listChildren()[0];
+			const sceneBounds = bounds(scene);
+			return {
+				name: scene.getName(),
+				rootName: root ? root.getName() : '',
+				bboxMin: toPrecision(sceneBounds.min),
+				bboxMax: toPrecision(sceneBounds.max),
+			};
+		});
+	return { properties: scenes };
 }
 
 /** List meshes. */
-function listMeshes (doc: Document): InspectPropertyReport<InspectMeshReport> {
-	const meshes: InspectMeshReport[] = doc.getRoot().listMeshes().map((mesh) => {
-		const instances = mesh.listParents()
-			.filter((parent) => parent.propertyType !== 'Root')
-			.length;
-		let glPrimitives = 0;
-		let verts = 0;
-		const semantics = new Set<string>();
-		const meshIndices = new Set<string>();
-		const meshAccessors: Set<Accessor> = new Set();
+function listMeshes(doc: Document): InspectPropertyReport<InspectMeshReport> {
+	const meshes: InspectMeshReport[] = doc
+		.getRoot()
+		.listMeshes()
+		.map((mesh) => {
+			const instances = mesh.listParents().filter((parent) => parent.propertyType !== PropertyType.ROOT).length;
+			let glPrimitives = 0;
+			let verts = 0;
+			const semantics = new Set<string>();
+			const meshIndices = new Set<string>();
+			const meshAccessors: Set<Accessor> = new Set();
 
-		mesh.listPrimitives().forEach((prim) => {
-			for (const semantic of prim.listSemantics()) {
-				const attr = prim.getAttribute(semantic)!;
-				semantics.add(semantic + ':' + arrayToType(attr.getArray()!));
-				meshAccessors.add(attr);
-			}
-			for (const targ of prim.listTargets()) {
-				targ.listAttributes().forEach((attr) => meshAccessors.add(attr));
-			}
-			const indices = prim.getIndices();
-			if (indices) {
-				meshIndices.add(arrayToType(indices.getArray()!));
-				meshAccessors.add(indices);
-			}
-			verts += prim.listAttributes()[0].getCount();
-			glPrimitives += getGLPrimitiveCount(prim);
+			mesh.listPrimitives().forEach((prim) => {
+				for (const semantic of prim.listSemantics()) {
+					const attr = prim.getAttribute(semantic)!;
+					semantics.add(semantic + ':' + arrayToType(attr.getArray()!));
+					meshAccessors.add(attr);
+				}
+				for (const targ of prim.listTargets()) {
+					targ.listAttributes().forEach((attr) => meshAccessors.add(attr));
+				}
+				const indices = prim.getIndices();
+				if (indices) {
+					meshIndices.add(arrayToType(indices.getArray()!));
+					meshAccessors.add(indices);
+				}
+				verts += prim.listAttributes()[0].getCount();
+				glPrimitives += getGLPrimitiveCount(prim);
+			});
+
+			let size = 0;
+			Array.from(meshAccessors).forEach((a) => (size += a.getArray()!.byteLength));
+
+			const modes = mesh.listPrimitives().map((prim) => MeshPrimitiveModeLabels[prim.getMode()]);
+
+			return {
+				name: mesh.getName(),
+				mode: Array.from(new Set(modes)),
+				primitives: mesh.listPrimitives().length,
+				glPrimitives: glPrimitives,
+				vertices: verts,
+				indices: Array.from(meshIndices).sort(),
+				attributes: Array.from(semantics).sort(),
+				instances: instances,
+				size: size,
+			};
 		});
 
-		let size = 0;
-		Array.from(meshAccessors).forEach((a) => (size += a.getArray()!.byteLength));
-
-		const modes = mesh.listPrimitives()
-			.map((prim) => MeshPrimitiveModeLabels[prim.getMode()]);
-
-		return {
-			name: mesh.getName(),
-			mode: Array.from(new Set(modes)),
-			primitives: mesh.listPrimitives().length,
-			glPrimitives: glPrimitives,
-			vertices: verts,
-			indices: Array.from(meshIndices).sort(),
-			attributes: Array.from(semantics).sort(),
-			instances: instances,
-			size: size,
-		};
-	});
-
-	return {properties: meshes};
+	return { properties: meshes };
 }
 
 /** List materials. */
-function listMaterials (doc: Document): InspectPropertyReport<InspectMaterialReport> {
-	const materials: InspectMaterialReport[] = doc.getRoot().listMaterials().map((material) => {
-		const instances = material.listParents()
-			.filter((parent) => parent.propertyType !== 'Root')
-			.length;
+function listMaterials(doc: Document): InspectPropertyReport<InspectMaterialReport> {
+	const materials: InspectMaterialReport[] = doc
+		.getRoot()
+		.listMaterials()
+		.map((material) => {
+			const instances = material
+				.listParents()
+				.filter((parent) => parent.propertyType !== PropertyType.ROOT).length;
 
-		// Find all texture slots attached to this material or its extensions.
-		const extensions = new Set<ExtensionProperty>(material.listExtensions());
-		const slots = doc.getGraph().listEdges()
-			.filter((ref) => {
-				const child = ref.getChild();
-				const parent = ref.getParent();
-				if (child instanceof Texture && parent === material) {
-					return true;
-				}
-				if (child instanceof Texture
-						&& parent instanceof ExtensionProperty
-						&& extensions.has(parent)) {
-					return true;
-				}
-				return false;
-			})
-			.map((ref) => ref.getName());
+			// Find all texture slots attached to this material or its extensions.
+			const extensions = new Set<ExtensionProperty>(material.listExtensions());
+			const slots = doc
+				.getGraph()
+				.listEdges()
+				.filter((ref) => {
+					const child = ref.getChild();
+					const parent = ref.getParent();
+					if (child instanceof Texture && parent === material) {
+						return true;
+					}
+					if (child instanceof Texture && parent instanceof ExtensionProperty && extensions.has(parent)) {
+						return true;
+					}
+					return false;
+				})
+				.map((ref) => ref.getName());
 
-		return {
-			name: material.getName(),
-			instances,
-			textures: slots,
-			alphaMode: material.getAlphaMode(),
-			doubleSided: material.getDoubleSided(),
-		};
-	});
+			return {
+				name: material.getName(),
+				instances,
+				textures: slots,
+				alphaMode: material.getAlphaMode(),
+				doubleSided: material.getDoubleSided(),
+			};
+		});
 
-	return {properties: materials};
+	return { properties: materials };
 }
 
 /** List textures. */
-function listTextures (doc: Document): InspectPropertyReport<InspectTextureReport> {
-	const textures: InspectTextureReport[] = doc.getRoot().listTextures().map((texture) => {
-		const instances = texture.listParents()
-			.filter((parent) => parent.propertyType !== 'Root')
-			.length;
+function listTextures(doc: Document): InspectPropertyReport<InspectTextureReport> {
+	const textures: InspectTextureReport[] = doc
+		.getRoot()
+		.listTextures()
+		.map((texture) => {
+			const instances = texture
+				.listParents()
+				.filter((parent) => parent.propertyType !== PropertyType.ROOT).length;
 
-		const slots = doc.getGraph().listParentEdges(texture)
-			.map((edge) => edge.getName())
-			.filter((name) => name !== 'texture');
+			const slots = doc
+				.getGraph()
+				.listParentEdges(texture)
+				.filter((edge) => edge.getParent().propertyType !== PropertyType.ROOT)
+				.map((edge) => edge.getName());
 
-		const resolution = ImageUtils.getSize(texture.getImage()!, texture.getMimeType());
+			const resolution = ImageUtils.getSize(texture.getImage()!, texture.getMimeType());
 
-		return {
-			name: texture.getName(),
-			uri: texture.getURI(),
-			slots: Array.from(new Set(slots)),
-			instances,
-			mimeType: texture.getMimeType(),
-			resolution: resolution ? resolution.join('x') : '',
-			size: texture.getImage()!.byteLength,
-			gpuSize: ImageUtils.getMemSize(texture.getImage()!, texture.getMimeType()),
-		};
-	});
+			return {
+				name: texture.getName(),
+				uri: texture.getURI(),
+				slots: Array.from(new Set(slots)),
+				instances,
+				mimeType: texture.getMimeType(),
+				resolution: resolution ? resolution.join('x') : '',
+				size: texture.getImage()!.byteLength,
+				gpuSize: ImageUtils.getMemSize(texture.getImage()!, texture.getMimeType()),
+			};
+		});
 
-	return {properties: textures};
+	return { properties: textures };
 }
 
 /** List animations. */
-function listAnimations (doc: Document): InspectPropertyReport<InspectAnimationReport> {
-	const animations: InspectAnimationReport[] = doc.getRoot().listAnimations().map((anim) => {
-		let minTime = Infinity;
-		let maxTime = -Infinity;
-		anim.listSamplers().forEach((sampler) => {
-			const input = sampler.getInput();
-			if (!input) return;
-			minTime = Math.min(minTime, input.getMin([])[0]);
-			maxTime = Math.max(maxTime, input.getMax([])[0]);
+function listAnimations(doc: Document): InspectPropertyReport<InspectAnimationReport> {
+	const animations: InspectAnimationReport[] = doc
+		.getRoot()
+		.listAnimations()
+		.map((anim) => {
+			let minTime = Infinity;
+			let maxTime = -Infinity;
+			anim.listSamplers().forEach((sampler) => {
+				const input = sampler.getInput();
+				if (!input) return;
+				minTime = Math.min(minTime, input.getMin([])[0]);
+				maxTime = Math.max(maxTime, input.getMax([])[0]);
+			});
+
+			let size = 0;
+			let keyframes = 0;
+			const accessors: Set<Accessor> = new Set();
+			anim.listSamplers().forEach((sampler) => {
+				const input = sampler.getInput();
+				const output = sampler.getOutput();
+				if (!input) return;
+				keyframes += input.getCount();
+				accessors.add(input);
+				if (!output) return;
+				accessors.add(output);
+			});
+			Array.from(accessors).forEach((accessor) => {
+				size += accessor.getArray()!.byteLength;
+			});
+
+			return {
+				name: anim.getName(),
+				channels: anim.listChannels().length,
+				samplers: anim.listSamplers().length,
+				duration: Math.round((maxTime - minTime) * 1000) / 1000,
+				keyframes: keyframes,
+				size: size,
+			};
 		});
 
-		let size = 0;
-		let keyframes = 0;
-		const accessors: Set<Accessor> = new Set();
-		anim.listSamplers().forEach((sampler) => {
-			const input = sampler.getInput();
-			const output = sampler.getOutput();
-			if (!input) return;
-			keyframes += input.getCount();
-			accessors.add(input);
-			if (!output) return;
-			accessors.add(output);
-		});
-		Array.from(accessors).forEach((accessor) => {
-			size += accessor.getArray()!.byteLength;
-		});
-
-		return {
-			name: anim.getName(),
-			channels: anim.listChannels().length,
-			samplers: anim.listSamplers().length,
-			duration: Math.round((maxTime - minTime) * 1000) / 1000,
-			keyframes: keyframes,
-			size: size,
-		};
-	});
-
-	return {properties: animations};
+	return { properties: animations };
 }
 
 export interface InspectReport {
