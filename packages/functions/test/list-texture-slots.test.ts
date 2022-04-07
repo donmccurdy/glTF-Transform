fb915b626d93d087eba905bf756bb55df6d5e27c
Don McCurdy <dm@donmccurdy.com>
2022-04-07 19:13:15 -0400

    [functions] Add tests for listTextureChannels, listTextureSlots

--- packages/functions/test/list-texture-slots.test.ts
+++ packages/functions/test/list-texture-slots.test.ts
@@ -1,8 +1,18 @@
 require('source-map-support').install();
 
 import test from 'tape';
+import { Document } from '@gltf-transform/core';
+import { listTextureSlots } from '@gltf-transform/functions';
+import { MaterialsSheen } from '@gltf-transform/extensions';
 
 test('@gltf-transform/functions::listTextureSlots', (t) => {
-	// TODO(test)
+	const document = new Document();
+	const textureA = document.createTexture();
+	const textureB = document.createTexture();
+	const sheenExtension = document.createExtension(MaterialsSheen);
+	const sheen = sheenExtension.createSheen().setSheenColorTexture(textureB);
+	document.createMaterial().setBaseColorTexture(textureA).setExtension('KHR_materials_sheen', sheen);
+	t.deepEquals(listTextureSlots(document, textureA), ['baseColorTexture'], 'baseColorTexture');
+	t.deepEquals(listTextureSlots(document, textureB), ['sheenColorTexture'], 'sheenColorTexture');
 	t.end();
 });
