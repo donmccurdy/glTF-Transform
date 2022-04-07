68219a1a25b795c1f2a96f2eb53f0b60bbd9b75c
Don McCurdy <dm@donmccurdy.com>
2022-04-07 15:41:32 -0400

    [functions] Placeholder tests for listTextureSlots, listTextureChannels

---
+++ packages/functions/test/list-texture-channels.test.ts
@@ -0,0 +1,13 @@
+require('source-map-support').install();
+
+import test from 'tape';
+
+test('@gltf-transform/functions::listTextureChannels', (t) => {
+	// TODO(test)
+	t.end();
+});
+
+test('@gltf-transform/functions::getTextureChannelMask', (t) => {
+	// TODO(test)
+	t.end();
+});
---
+++ packages/functions/test/list-texture-slots.test.ts
@@ -0,0 +1,8 @@
+require('source-map-support').install();
+
+import test from 'tape';
+
+test('@gltf-transform/functions::listTextureSlots', (t) => {
+	// TODO(test)
+	t.end();
+});
