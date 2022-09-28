4177b0a4e0fb5ee0c3d02fc0dd5db4990a2b55a0
Don McCurdy <dm@donmccurdy.com>
2022-09-28 08:17:18 -0400

    [functions] Add listTextureInfo (#692)

---
+++ packages/functions/src/list-texture-info.ts
@@ -0,0 +1,34 @@
+import { Document, Texture, TextureInfo } from '@gltf-transform/core';
+
+/**
+ * Lists all {@link TextureInfo} definitions associated with a given {@link Texture}.
+ *
+ * Example:
+ *
+ * ```js
+ * // Find TextureInfo instances associated with the texture.
+ * const results = listTextureInfo(document, texture);
+ *
+ * // Find which UV sets (TEXCOORD_0, TEXCOORD_1, ...) are required.
+ * const texCoords = results.map((info) => info.getTexCoord());
+ * // → [0, 0, 1]
+ * ```
+ */
+export function listTextureInfo(document: Document, texture: Texture): TextureInfo[] {
+	const graph = document.getGraph();
+	const results: TextureInfo[] = [];
+
+	for (const textureEdge of graph.listParentEdges(texture)) {
+		const parent = textureEdge.getParent();
+		const name = textureEdge.getName() + 'Info';
+
+		for (const edge of graph.listChildEdges(parent)) {
+			const child = edge.getChild();
+			if (child instanceof TextureInfo && edge.getName() === name) {
+				results.push(child);
+			}
+		}
+	}
+
+	return results;
+}
