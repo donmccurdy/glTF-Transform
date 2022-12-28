b85968d1921f4fe98b1be4e64e8229701bda3f7c
Don McCurdy <dm@donmccurdy.com>
2022-12-27 23:50:08 -0500

    Add tests

--- packages/functions/test/resample.test.ts
+++ packages/functions/test/resample.test.ts
@@ -2,6 +2,7 @@ require('source-map-support').install();
 
 import test from 'tape';
 import { Accessor, Document, Logger } from '@gltf-transform/core';
+import { fromEuler } from 'gl-matrix/quat';
 import { resample } from '../';
 
 test('@gltf-transform/functions::resample', async (t) => {
@@ -39,6 +40,43 @@ test('@gltf-transform/functions::resample', async (t) => {
 	// No change.
 	t.deepEquals(toArray(samplerC.getInput()), Array.from(inArray), 'CUBICSPLINE input (unchanged)');
 	t.deepEquals(toArray(samplerC.getOutput()), Array.from(outSplineArray), 'CUBICSPLINE output (unchanged)');
+	t.end();
+});
+
+test('@gltf-transform/functions::resample | rotation', async (t) => {
+	const doc = new Document().setLogger(new Logger(Logger.Verbosity.SILENT));
+
+	const inArray = new Uint8Array([0, 1, 2, 3, 4, 5, 6]);
+	const outArray = new Float32Array([
+		...fromEuler([], 0, 0, 0),
+		...fromEuler([], 45, 0, 0),
+		...fromEuler([], 90, 0, 0),
+		...fromEuler([], 135, 0, 0),
+		...fromEuler([], 180, 0, 0), // resampling can't create ≥180º steps!
+		...fromEuler([], 225, 0, 0),
+		...fromEuler([], 270, 0, 0),
+	]);
+
+	const input = doc.createAccessor('input').setType(Accessor.Type.SCALAR).setArray(inArray);
+	const output = doc.createAccessor('output').setType(Accessor.Type.VEC4).setArray(outArray);
+	const sampler = doc.createAnimationSampler().setInterpolation('LINEAR').setInput(input).setOutput(output);
+	const channel = doc.createAnimationChannel().setTargetPath('rotation').setSampler(sampler);
+	doc.createAnimation().addChannel(channel).addSampler(sampler);
+
+	await doc.transform(resample());
+
+	t.deepEquals(toArray(sampler.getInput()), [0, 3, 6], 'input');
+	t.deepEquals(
+		toArray(sampler.getOutput()),
+		// prettier-ignore
+		[
+			0, 0, 0, 1,
+			0.9238795042037964, 0, 0, 0.3826834261417389,
+			0.7071067690849304, 0, -0, -0.7071067690849304
+		],
+		'output'
+	);
+
 	t.end();
 });
 
