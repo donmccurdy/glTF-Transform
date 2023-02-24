f71d1905ca6ef535df00e4e3608d17359ea52578
Don McCurdy <dm@donmccurdy.com>
2023-02-24 16:28:57 -0500

    Fix broken test.

--- packages/functions/src/get-node-scene.ts
+++ packages/functions/src/get-node-scene.ts
@@ -3,5 +3,5 @@ import { listNodeScenes } from './list-node-scenes.js';
 
 /** @deprecated Use {@link listNodeScenes} instead. */
 export function getNodeScene(node: Node): Scene | null {
-	return listNodeScenes(node)[0];
+	return listNodeScenes(node)[0] || null;
 }
