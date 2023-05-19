47fda975798b99e8c01b6fa5fa1f702e691445f3
Don McCurdy <dm@donmccurdy.com>
2023-05-19 17:04:29 -0400

    Remove source-map-support dependency.

--- packages/cli/bin/cli.js
+++ packages/cli/bin/cli.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-// Uncomment for better stack traces during local development.
-// require('source-map-support').install();
-
 import { program, programReady } from '../dist/cli.esm.js';
 
 program.disableGlobalOption('--silent');
