15805fb956ccd17c544fd764a18b5905ef4eab58
Don McCurdy <dm@donmccurdy.com>
2023-11-12 10:21:48 -0500

    feat(functions): Enable prune() to remove redundant mesh indices

--- packages/functions/src/prune.ts
+++ packages/functions/src/prune.ts
@@ -39,6 +39,8 @@ export interface PruneOptions {
 	keepLeaves?: boolean;
 	/** Whether to keep unused vertex attributes, such as UVs without an assigned texture. */
 	keepAttributes?: boolean;
+	/** Whether to keep redundant mesh indices, where vertex count equals index count. */
+	keepIndices?: boolean;
 	/** Whether to keep single-color textures that can be converted to material factors. */
 	keepSolidTextures?: boolean;
 }
@@ -58,6 +60,7 @@ const PRUNE_DEFAULTS: Required<PruneOptions> = {
 	],
 	keepLeaves: false,
 	keepAttributes: true,
+	keepIndices: false,
 	keepSolidTextures: true,
 };
 
@@ -160,6 +163,15 @@ export function prune(_options: PruneOptions = PRUNE_DEFAULTS): Transform {
 			}
 			for (const [material, prims] of materialPrims) {
 				shiftTexCoords(material, Array.from(prims));
+			}
+		}
+
+		// Prune unused mesh indices.
+		if (!options.keepIndices && propertyTypes.has(PropertyType.ACCESSOR)) {
+			for (const mesh of root.listMeshes()) {
+				for (const prim of mesh.listPrimitives()) {
+					pruneIndices(prim);
+				}
 			}
 		}
 
@@ -297,6 +309,14 @@ function nodeTreeShake(graph: Graph<Property>, prop: Node | Scene, counter: Disp
 function pruneAttributes(prim: Primitive | PrimitiveTarget, unused: string[]) {
 	for (const semantic of unused) {
 		prim.setAttribute(semantic, null);
+	}
+}
+
+function pruneIndices(prim: Primitive) {
+	const indices = prim.getIndices();
+	const attribute = prim.listAttributes()[0];
+	if (indices && attribute && indices.getCount() === attribute.getCount()) {
+		prim.setIndices(null);
 	}
 }
 
