e295b4ad42a923ac9d3ee442ee6d7681329f8d73
Don McCurdy <dm@donmccurdy.com>
2024-03-24 15:16:35 -0400

    Tests for joining line-strip, line-loop, triangle-strip, and triangle-fan

--- packages/functions/test/join.test.ts
+++ packages/functions/test/join.test.ts
@@ -1,11 +1,22 @@
 import test from 'ava';
-import { getBounds, Document } from '@gltf-transform/core';
-import { join, quantize } from '@gltf-transform/functions';
-import { createPlatformIO, logger, roundBbox } from '@gltf-transform/test-utils';
+import { getBounds, Document, Primitive } from '@gltf-transform/core';
+import { join, quantize, transformPrimitive } from '@gltf-transform/functions';
+import {
+	createLineLoopPrim,
+	createLineStripPrim,
+	createPlatformIO,
+	createTriangleFanPrim,
+	createTriangleStripPrim,
+	logger,
+	roundBbox,
+	mat4,
+} from '@gltf-transform/test-utils';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const { LINES, TRIANGLES } = Primitive.Mode;
 
 test('basic', async (t) => {
 	const io = await createPlatformIO();
@@ -50,4 +61,37 @@ test('no side effects', async (t) => {
 
 	t.is(document.getRoot().listNodes().length, 2, 'skips prune');
 	t.is(document.getRoot().listAccessors().length, 2, 'skips dedup');
+});
+
+test('primitive modes', async (t) => {
+	const document = new Document().setLogger(logger);
+	const primLineStrip = createLineStripPrim(document);
+	const primLineLoop = createLineLoopPrim(document);
+	const primTriangleStrip = createTriangleStripPrim(document);
+	const primTriangleFan = createTriangleFanPrim(document);
+	const mesh = document
+		.createMesh()
+		.addPrimitive(primLineStrip)
+		.addPrimitive(primLineLoop)
+		.addPrimitive(primTriangleStrip)
+		.addPrimitive(primTriangleFan);
+	const node = document.createNode().setMesh(mesh);
+	document.createScene().addChild(node);
+
+	transformPrimitive(primLineStrip, mat4.fromTranslation([], [-4, 0, 0]));
+	transformPrimitive(primLineLoop, mat4.fromTranslation([], [-2, 0, 0]));
+	transformPrimitive(primTriangleStrip, mat4.fromTranslation([], [2, 0, 0]));
+	transformPrimitive(primTriangleFan, mat4.fromTranslation([], [4, 0, 0]));
+
+	await document.transform(join());
+
+	t.true(primLineStrip.isDisposed(), 'line-strip disposed');
+	t.true(primLineLoop.isDisposed(), 'line-loop disposed');
+	t.true(primTriangleStrip.isDisposed(), 'triangle-strip disposed');
+	t.true(primTriangleFan.isDisposed(), 'triangle-fan disposed');
+
+	t.false(mesh.isDisposed(), 'mesh not disposed');
+	t.is(mesh.listPrimitives().length, 2, 'mesh has two (2) prims');
+	t.is(mesh.listPrimitives()[0].getMode(), LINES, 'joins line-strip and line-loop');
+	t.is(mesh.listPrimitives()[1].getMode(), TRIANGLES, 'joins triangle-strip and triangle-fan');
 });
