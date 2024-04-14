c5c41f5fcbfa1ffd6b14e0c11211ad042832df37
Don McCurdy <dm@donmccurdy.com>
2024-04-14 17:01:28 -0400

    perf(functions): Optimize reorder() for shared vertex streams

--- packages/functions/src/reorder.ts
+++ packages/functions/src/reorder.ts
@@ -1,8 +1,8 @@
 import { Accessor, Document, GLTF, Primitive, PropertyType, Transform } from '@gltf-transform/core';
 import { prune } from './prune.js';
 import { createTransform, deepListAttributes, SetMap, shallowCloneAccessor } from './utils.js';
-import { remapAttribute } from './remap-primitive.js';
 import type { MeshoptEncoder } from 'meshoptimizer';
+import { compactAttribute } from './compact-primitive.js';
 
 const NAME = 'reorder';
 
@@ -87,7 +87,7 @@ export function reorder(_options: ReorderOptions): Transform {
 			// Update affected primitives.
 			for (const srcAttribute of plan.indicesToAttributes.get(srcIndices)) {
 				const dstAttribute = shallowCloneAccessor(document, srcAttribute);
-				remapAttribute(dstAttribute, remap, unique);
+				compactAttribute(srcAttribute, srcIndices, remap, dstAttribute, unique);
 				for (const prim of plan.attributesToPrimitives.get(srcAttribute)) {
 					if (prim.getIndices() === srcIndices) {
 						prim.swap(srcIndices, dstIndices);
