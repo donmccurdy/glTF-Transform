7ad3a7ad0533ccd6f635bfdff37a5e21fc5b4f12
Li Yanxu <halyx@126.com>
2025-09-22 11:31:46 +0800

    feat: primive-outline

---
+++ packages/functions/src/primitive-outline.ts
@@ -0,0 +1,135 @@
+import { type Document, Primitive, Triangle, type vec3 } from '@gltf-transform/core';
+import { vec3 as glvec3 } from 'gl-matrix';
+import { assignDefaults, createTransform } from './utils.js';
+
+const NAME = 'primitive-outline';
+
+export interface PrimitiveOutlineOptions {
+	thresholdAngel: number;
+}
+
+export const PRIMITIVE_OUTLINE_DEFAULTS: Required<PrimitiveOutlineOptions> = {
+	thresholdAngel: 0.05,
+};
+
+export function createPrimitiveOutline(_option: PrimitiveOutlineOptions = PRIMITIVE_OUTLINE_DEFAULTS): Primitive {
+	const options = assignDefaults(PRIMITIVE_OUTLINE_DEFAULTS, _option);
+
+	return createTransform(NAME, (doc: Document) => {
+		const meshes = doc.getRoot().listMeshes();
+		for (const mesh of meshes) {
+			const srcPrims = mesh.listPrimitives();
+			for (const prim of srcPrims) {
+				mesh.addPrimitive(createEdgePrim(doc, prim, options));
+			}
+		}
+	});
+}
+
+function createEdgePrim(document: Document, prim: Primitive, options: PrimitiveOutlineOptions): Primitive {
+	const positionAccessor = prim.getAttribute('POSITION');
+	const positions = positionAccessor?.getArray();
+	const indices = prim.getIndices()?.getArray();
+	const indexCount = indices ? indices.length : positions!.length / 3;
+	const precisionCount = 4;
+	const precision = Math.pow(10, precisionCount);
+
+	const thresholdAngle = options.thresholdAngle;
+
+	const indexArr = [0, 0, 0];
+	const vertKeys = ['a', 'b', 'c'];
+	const triangle = new Triangle();
+	const hashes = new Array(3);
+	const edgeData = {};
+	const vertices = [];
+
+	for (let i = 0; i < indexCount; i += 3) {
+		if (indices) {
+			indexArr[0] = indices[i * 3 + 0];
+			indexArr[1] = indices[i * 3 + 1];
+			indexArr[2] = indices[i * 3 + 2];
+		} else {
+			indexArr[0] = i * 3 + 0;
+			indexArr[1] = i * 3 + 1;
+			indexArr[2] = i * 3 + 2;
+		}
+		const _triangle = new Triangle(
+			[positions[indexArr[0] * 3 + 0], positions[indexArr[0] * 3 + 1], positions[indexArr[0] * 3 + 2]] as vec3,
+			[positions[indexArr[1] * 3 + 0], positions[indexArr[1] * 3 + 1], positions[indexArr[1] * 3 + 2]] as vec3,
+			[positions[indexArr[2] * 3 + 0], positions[indexArr[2] * 3 + 1], positions[indexArr[2] * 3 + 2]] as vec3,
+		);
+		const { a, b, c } = _triangle;
+		const normal = [0, 0, 0] as vec3;
+		Triangle.getNormal(a, b, c, normal);
+
+		hashes[0] = `${Math.round(a[0] * precision)},${Math.round(a[1] * precision)},${Math.round(a[2] * precision)}`;
+		hashes[1] = `${Math.round(b[0] * precision)},${Math.round(b[1] * precision)},${Math.round(b[2] * precision)}`;
+		hashes[2] = `${Math.round(c[0] * precision)},${Math.round(c[1] * precision)},${Math.round(c[2] * precision)}`;
+
+		// skip degenerate triangles
+		if (hashes[0] === hashes[1] || hashes[1] === hashes[2] || hashes[2] === hashes[0]) {
+			continue;
+		}
+
+		for (let j = 0; j < 3; j++) {
+			const jNext = (j + 1) % 3;
+			const vecHash0 = hashes[j];
+			const vecHash1 = hashes[jNext];
+			const v0 = _triangle[vertKeys[j]];
+			const v1 = _triangle[vertKeys[jNext]];
+			const hash = `${vecHash0}_${vecHash1}`;
+			const reverseHash = `${vecHash1}_${vecHash0}`;
+			if (reverseHash in edgeData && edgeData[reverseHash]) {
+				// if we found a sibling edge add it into the vertex array if
+				// it meets the angle threshold and delete the edge from the map.
+				if (glvec3.dot(normal, edgeData[reverseHash].normal) <= thresholdAngle) {
+					vertices.push(v0[0], v0[1], v0[2]);
+					vertices.push(v1[0], v1[1], v1[2]);
+				}
+
+				edgeData[reverseHash] = null;
+			} else if (!(hash in edgeData)) {
+				// if we've already got an edge here then skip adding a new one
+				edgeData[hash] = {
+					index0: indexArr[j],
+					index1: indexArr[jNext],
+					normal: glvec3.clone(normal),
+				};
+			}
+		}
+	}
+
+	// iterate over all remaining, unmatched edges and add them to the vertex array
+	for (const key in edgeData) {
+		if (edgeData[key]) {
+			const { index0, index1 } = edgeData[key];
+			const v0 = glvec3.fromValues(
+				positions[index0 * 3 + 0],
+				positions[index0 * 3 + 1],
+				positions[index0 * 3 + 2],
+			);
+			const v1 = glvec3.fromValues(
+				positions[index1 * 3 + 0],
+				positions[index1 * 3 + 1],
+				positions[index1 * 3 + 2],
+			);
+
+			vertices.push(v0[0], v0[1], v0[2]);
+			vertices.push(v1[0], v1[1], v1[2]);
+		}
+	}
+
+	const accesor = document
+		.createAccessor()
+		.setArray(new Float32Array(vertices))
+		.setType('VEC3')
+		.setBuffer(positionAccessor.getBuffer());
+
+	const edgePrim = document
+		.createPrimitive()
+		.setName(prim.getName() + '_edges')
+		.setMode(Primitive.Mode.LINES)
+		.setAttribute('POSITION', accesor);
+
+	return edgePrim;
+}
