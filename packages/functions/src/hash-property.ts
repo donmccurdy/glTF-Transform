507ebb38e12f801d47c4aba46b2dce05421381cc
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2025-12-04 22:09:49 -0500

    consolidate hashers

--- packages/functions/src/hash-property.ts
+++ packages/functions/src/hash-property.ts
@@ -1,5 +1,6 @@
 import { BufferUtils, type Property } from '@gltf-transform/core';
 import { $attributes, GraphEdge, type Literal, type Ref, RefList, RefMap, RefSet } from 'property-graph';
+import { murmurHash2 } from './utils/hash-utils.js';
 import { isPlainObject } from './utils/object-utils.js';
 
 const EMPTY_SET = new Set<string>();
@@ -141,27 +142,4 @@ function _hashBytes(value: Uint8Array): number {
 	const bytes = BufferUtils.pad(value);
 	const bytesU32 = new Uint32Array(bytes.buffer, bytes.byteOffset, bytes.byteLength / Uint32Array.BYTES_PER_ELEMENT);
 	return murmurHash2(0, bytesU32);
-}
-
-/**
- * References:
- * - https://github.com/mikolalysenko/murmurhash-js/blob/f19136e9f9c17f8cddc216ca3d44ec7c5c502f60/murmurhash2_gc.js#L14
- * - https://github.com/zeux/meshoptimizer/blob/e47e1be6d3d9513153188216455bdbed40a206ef/src/indexgenerator.cpp#L12
- */
-function murmurHash2(h: number, key: Uint32Array): number {
-	const m = 0x5bd1e995;
-	const r = 24;
-
-	for (let i = 0, il = key.length; i < il; i++) {
-		let k = key[i];
-
-		k = Math.imul(k, m) >>> 0;
-		k = (k ^ (k >> r)) >>> 0;
-		k = Math.imul(k, m) >>> 0;
-
-		h = Math.imul(h, m) >>> 0;
-		h = (h ^ k) >>> 0;
-	}
-
-	return h;
 }
