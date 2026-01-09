aa1c8cf3133e9c64fa6a8d75a80b209a47765804
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-01-09 12:30:26 -0500

    docs

--- packages/extensions/src/khr-accessor-float16/accessor-float16.ts
+++ packages/extensions/src/khr-accessor-float16/accessor-float16.ts
@@ -4,7 +4,33 @@ import { KHR_ACCESSOR_FLOAT16 } from '../constants.js';
 const NAME = KHR_ACCESSOR_FLOAT16;
 
 /**
- * TODO
+ * [`KHR_accessor_float16`](https://github.com/donmccurdy/glTF-Transform/pull/1417) enables support
+ * for IEEE-754 binary16 ("half-precision" or "float16") {@link Accessor Accessors}, in all
+ * contexts where the base glTF 2.0 specification already allows binary32 ("single-precision" or
+ * "float32") Accessors. This extension must be marked as required; implementations that do not
+ * support float16 Accessors will be unable to parse the Document correctly.
+ *
+ * ### Example
+ *
+ * ```typescript
+ * import {Accessor} from '@gltf-transform/core';
+ * import {KHRAccessorFloat16} from '@gltf-transform/extensions';
+ *
+ * // Create an Extension attached to the Document.
+ * document.createExtension(KHRAccessorFloat16).setRequired(true);
+ *
+ * // Store Float16Array instances in one or more Accessors.
+ * const mesh = document.getRoot().listMeshes()
+ *   .find((mesh) => mesh.getName() === 'MyMesh');
+ *
+ * for (const prim of mesh.listPrimitives()) {
+ *   const position = document.createAccessor()
+ *     .setType('VEC3')
+ *     .setArray(new Float16Array([...]));
+ *   prim.setAttribute('POSITION', position);
+ * }
+ * ```
+ *
  * @experimental KHR_accessor_float16 is not yet ratified by the Khronos Group.
  */
 export class KHRAccessorFloat16 extends Extension {
