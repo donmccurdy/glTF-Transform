4e2dc1151741ac3b21ad77ef775a5a9fd02435cd
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-01-09 13:48:06 -0500

    add basic unit tests

---
+++ packages/extensions/test/accessor-float16.test.ts
@@ -0,0 +1,49 @@
+import { Accessor, Document, NodeIO } from '@gltf-transform/core';
+import { KHRAccessorFloat16 } from '@gltf-transform/extensions';
+import { cloneDocument } from '@gltf-transform/functions';
+import test from 'ava';
+
+const WRITER_OPTIONS = { basename: 'extensionTest' };
+
+test('basic', async (t) => {
+	// TODO(v5): Remove after Node.js v22 reaches EOL, or adds Float16Array support.
+	if (typeof Float16Array === 'undefined') t.pass();
+
+	const document = new Document();
+	const io = new NodeIO().registerExtensions([KHRAccessorFloat16]);
+
+	document.createExtension(KHRAccessorFloat16).setRequired(true);
+
+	const buffer = document.createBuffer();
+	const position = document
+		.createAccessor()
+		.setType('VEC3')
+		.setArray(new Float16Array([0, 1, 2]))
+		.setBuffer(buffer);
+	const prim = document.createPrimitive().setAttribute('POSITION', position);
+	document.createMesh().addPrimitive(prim);
+
+	const jsonDoc = await io.writeJSON(document, WRITER_OPTIONS);
+	t.deepEqual(jsonDoc.json.extensionsUsed, [KHRAccessorFloat16.EXTENSION_NAME], 'writes extensionsUsed');
+	t.deepEqual(jsonDoc.json.extensionsRequired, [KHRAccessorFloat16.EXTENSION_NAME], 'writes extensionsRequired');
+
+	const rtDocument = await io.readJSON(jsonDoc);
+
+	const rtExtensions = rtDocument.getRoot().listExtensionsRequired();
+	const rtExtension = rtExtensions.find((ext) => ext.extensionName === 'KHR_accessor_float16');
+	t.truthy(rtExtension, 'reads KHR_accessor_float16');
+
+	const rtPrim = rtDocument.getRoot().listMeshes()[0].listPrimitives()[0];
+	const rtAccessor = rtPrim.getAttribute('POSITION');
+
+	t.is(rtAccessor.getComponentType(), Accessor.ComponentType.FLOAT16, 'componentType == FLOAT16');
+	t.true(rtAccessor.getArray() instanceof Float16Array, 'array instanceof Float16Array');
+	t.deepEqual(Array.from(rtAccessor.getArray()), [0, 1, 2], 'array contents');
+});
+
+test('copy', (t) => {
+	const document = new Document();
+	document.createExtension(KHRAccessorFloat16).setRequired(true);
+
+	t.is(cloneDocument(document).getRoot().listExtensionsUsed().length, 1, 'copy KHRAccessorFloat16');
+});
