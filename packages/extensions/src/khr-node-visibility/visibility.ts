815192de26b9b1373daa390ddd4d94b9e1378ffb
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-04-15 16:23:59 -0400

    chore(extensions): Mark KHR_node_visibility as ratified

--- packages/extensions/src/khr-node-visibility/visibility.ts
+++ packages/extensions/src/khr-node-visibility/visibility.ts
@@ -7,8 +7,6 @@ interface IVisibility extends IProperty {
 
 /**
  * Defines visibility of a {@link Node} and its descendants. See {@link KHRNodeVisibility}.
- *
- * @experimental KHR_node_visibility is not yet ratified by the Khronos Group.
  */
 export class Visibility extends ExtensionProperty<IVisibility> {
 	public static EXTENSION_NAME: typeof KHR_NODE_VISIBILITY = KHR_NODE_VISIBILITY;
