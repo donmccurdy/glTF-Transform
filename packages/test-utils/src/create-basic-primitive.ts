445cf4570fc9445fd47ebd8fc8858979344c13d3
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-06-23 09:35:27 -0400

    chore(test-utils): Generate primitive topology samples with transforms (#1822)

--- packages/test-utils/src/create-basic-primitive.ts
+++ packages/test-utils/src/create-basic-primitive.ts
@@ -1,19 +1,27 @@
-import { type Document, Primitive } from '@gltf-transform/core';
+import { type Document, Primitive, type vec3 } from '@gltf-transform/core';
 
 const { LINE_STRIP, LINE_LOOP, TRIANGLE_STRIP, TRIANGLE_FAN } = Primitive.Mode;
+
+const DEFAULT_TRANSLATION: vec3 = [0, 0, 0];
+const DEFAULT_SCALE: vec3 = [1, 1, 1];
 
 /**
  * Creates a series of 'primCount' lines, forming an open-sided N-gon.
  *
  * Reference: https://glasnost.itcarlow.ie/~powerk/opengl/primitives/primitives.htm
  */
-export function createLineStripPrim(document: Document, primCount = 8): Primitive {
+export function createLineStripPrim(
+	document: Document,
+	primCount = 8,
+	translation = DEFAULT_TRANSLATION,
+	scale = DEFAULT_SCALE,
+): Primitive {
 	const vertexCount = primCount + 1;
 	const positionArray = new Float32Array(vertexCount * 3);
 	for (let i = 0; i < positionArray.length / 3; i++) {
-		positionArray[i * 3] = Math.cos((i / vertexCount) * 2 * Math.PI);
-		positionArray[i * 3 + 1] = Math.sin((i / vertexCount) * 2 * Math.PI);
-		positionArray[i * 3 + 2] = 0;
+		positionArray[i * 3] = translation[0] + Math.cos((i / vertexCount) * 2 * Math.PI) * scale[0];
+		positionArray[i * 3 + 1] = translation[1] + Math.sin((i / vertexCount) * 2 * Math.PI) * scale[1];
+		positionArray[i * 3 + 2] = translation[2];
 	}
 
 	const buffer = document.getRoot().listBuffers()[0] || document.createBuffer();
@@ -31,8 +39,13 @@ export function createLineStripPrim(document: Document, primCount = 8): Primitiv
  *
  * Reference: https://glasnost.itcarlow.ie/~powerk/opengl/primitives/primitives.htm
  */
-export function createLineLoopPrim(document: Document): Primitive {
-	return createLineStripPrim(document).setMode(LINE_LOOP);
+export function createLineLoopPrim(
+	document: Document,
+	primCount = 8,
+	translation = DEFAULT_TRANSLATION,
+	scale = DEFAULT_SCALE,
+): Primitive {
+	return createLineStripPrim(document, primCount, translation, scale).setMode(LINE_LOOP);
 }
 
 /**
@@ -41,13 +54,18 @@ export function createLineLoopPrim(document: Document): Primitive {
  *
  * Reference: https://en.wikipedia.org/wiki/Triangle_strip
  */
-export function createTriangleStripPrim(document: Document, primCount = 8): Primitive {
+export function createTriangleStripPrim(
+	document: Document,
+	primCount = 8,
+	translation = DEFAULT_TRANSLATION,
+	scale = DEFAULT_SCALE,
+): Primitive {
 	const vertexCount = primCount + 2;
 	const positionArray = new Float32Array(vertexCount * 3);
 	for (let i = 0; i < positionArray.length / 3; i++) {
-		positionArray[i * 3] = i % 2;
-		positionArray[i * 3 + 1] = Math.floor(i / 2);
-		positionArray[i * 3 + 2] = 0;
+		positionArray[i * 3] = translation[0] + (i % 2) * scale[0];
+		positionArray[i * 3 + 1] = translation[1] + Math.floor(i / 2) * scale[1];
+		positionArray[i * 3 + 2] = translation[2];
 	}
 
 	const buffer = document.getRoot().listBuffers()[0] || document.createBuffer();
@@ -66,13 +84,18 @@ export function createTriangleStripPrim(document: Document, primCount = 8): Prim
  *
  * Reference: https://en.wikipedia.org/wiki/Triangle_fan
  */
-export function createTriangleFanPrim(document: Document, primCount = 8): Primitive {
+export function createTriangleFanPrim(
+	document: Document,
+	primCount = 8,
+	translation = DEFAULT_TRANSLATION,
+	scale = DEFAULT_SCALE,
+): Primitive {
 	const vertexCount = primCount + 2;
 	const positionArray = new Float32Array(vertexCount * 3);
 	for (let i = 0; i < positionArray.length / 3; i++) {
-		positionArray[i * 3] = Math.cos((i / vertexCount) * 2 * Math.PI);
-		positionArray[i * 3 + 1] = Math.sin((i / vertexCount) * 2 * Math.PI);
-		positionArray[i * 3 + 2] = 0;
+		positionArray[i * 3] = translation[0] + Math.cos((i / vertexCount) * 2 * Math.PI) * scale[0];
+		positionArray[i * 3 + 1] = translation[1] + Math.sin((i / vertexCount) * 2 * Math.PI) * scale[1];
+		positionArray[i * 3 + 2] = translation[2];
 	}
 
 	const buffer = document.getRoot().listBuffers()[0] || document.createBuffer();
