5c68da00a6344fd4759c2163116e5e6d37ef523f
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-07-01 21:38:12 -0400

    fix(build): Minor fixes

---
+++ packages/extensions/tsdown.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'tsdown';
+import baseConfig from '../../tsdown.config.ts';
+
+export default defineConfig({
+	...baseConfig,
+	deps: { skipNodeModulesBundle: true },
+});
