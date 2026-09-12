53a89d43f1f96ef6ecf6a43893c5975fedbab3a6
Don McCurdy <1848368+donmccurdy@users.noreply.github.com>
2026-09-12 23:10:16 +0200

    debugging

--- packages/core/src/io/node-io.ts
+++ packages/core/src/io/node-io.ts
@@ -108,12 +108,18 @@ export class NodeIO extends PlatformIO {
 	}
 
 	protected resolve(base: string, path: string): string {
+		console.log(`resolving: "${base}", "${path}"`);
 		if (HTTPUtils.isAbsoluteURL(base) || HTTPUtils.isAbsoluteURL(path)) {
-			return HTTPUtils.resolve(base, path);
+			const httpResolve = HTTPUtils.resolve(base, path);
+			console.log(`HTTPUtils.resolve: "${httpResolve}"`);
+			return httpResolve;
 		}
+		const decoded = decodeURIComponent(path);
+		const resolved = this._path.resolve(base, decoded);
+		console.log(`resolved: "${decoded}", "${resolved}"`);
 		// https://github.com/KhronosGroup/glTF/issues/1449
 		// https://stackoverflow.com/a/27278490/1314762
-		return this._path.resolve(base, decodeURIComponent(path));
+		return resolved;
 	}
 
 	protected dirname(uri: string): string {
